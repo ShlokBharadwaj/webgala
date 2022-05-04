cd5115488388af723f94d73cecd275071b437174
Shlok Bharadwaj <shlokbharadwaj7@gmail.com>
2022-05-04 23:36:41 +0530

    Added image from API

--- Movie_App/script.js
+++ Movie_App/script.js
@@ -1,1 +1,3 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8f6a0fca12c89f86f46fe4da733ada3e&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+ 
