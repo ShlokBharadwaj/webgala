891730b21f5217246ad14dedb48f1a548c5bc3b9
Shlok Bharadwaj <shlokbharadwaj7@gmail.com>
2022-05-05 00:34:45 +0530

    Added search functionality

--- Movie_App/script.js
+++ Movie_App/script.js
@@ -1,32 +1,68 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8f6a0fca12c89f86f46fe4da733ada3e&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/discover/movie?api_key=8f6a0fca12c89f86f46fe4da733ada3e&query="'
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8f6a0fca12c89f86f46fe4da733ada3e&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=8f6a0fca12c89f86f46fe4da733ada3e&query="';
 
-const form = document.getElementById('form')
-const search = document.getElementById('search')
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
 
 // Get initial movies
-getMovies(API_URL)
+getMovies(API_URL);
 
 async function getMovies(url) {
-    const res  = await fetch(url)
-    const data = await res.json()
+  const res = await fetch(url);
+  const data = await res.json();
 
-    console.log(data.results)
+  showMovies(data.results);
 }
 
+function showMovies(movies) {
+  main.innerHTML = "";
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
 
-    const searchTerm = search.value
+    const movieEle = document.createElement("div");
+    movieEle.classList.add("movie");
 
-    if(searchTerm && searchTerm !== '') {
-        getMovies(SEARCH_API + searchTerm)
+    movieEle.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div id="overview" class="overview">
+          <h3>Overview</h3>
+         ${overview}
+        </div>
+        `;
 
-        search.value = ''
-    }else {
-        window.location.reload()
-    }
-})
+    main.appendChild(movieEle);
+  });
+}
 
+function getClassByRate(vote) {
+  if (vote >= 7.5) {
+    return "green";
+  } else if (vote >= 4.5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
