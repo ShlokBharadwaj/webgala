29e73d0b610f12043881d6f1edf0c76b023ce7a8
Shlok Bharadwaj <shlokbharadwaj7@gmail.com>
2022-05-13 18:43:22 +0530

    Add: Clock Functionality

--- Theme_Clock/script.js
+++ Theme_Clock/script.js
@@ -10,11 +10,39 @@ const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "
 
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
-    if(html.classList.contains('dark')) {
+    if (html.classList.contains('dark')) {
         html.classList.remove('dark')
-        e.target.innerHTML = 'Dark Mode'
+        e.target.innerHTML = 'Dark mode'
     } else {
         html.classList.add('dark')
-        e.target.innerHTML = 'Light Mode'
+        e.target.innerHTML = 'Light mode'
     }
 })
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth()
+    const day = time.getDay()
+    const date = time.getDate()
+    const hours = time.getHours()
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    const ampm = hours >= 12 ? 'PM' : 'AM'
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+setTime()
+
+setInterval(setTime, 1000)
