edf25273fecce84e2b43da2ab89cb9e2c2c8c01e
Shlok Bharadwaj <shlokbharadwaj7@gmail.com>
2022-06-01 18:55:22 +0530

    Add: Link for axios CDN

--- Github_Profiles/script.js
+++ Github_Profiles/script.js
@@ -1,3 +1,10 @@
- const APIURL = 'https://api.github.com/users/'
 
- 
+const APIURL = 'https://api.github.com/users'
+
+getUser('ShlokBharadwaj')
+
+function getUser(username) {
+    axios(APIURL + username)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+}
