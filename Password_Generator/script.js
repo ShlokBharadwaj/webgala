7613ee76412f8d17b0ac3a73e6c24b73d9064194
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-06-12 09:46:25 +0530

    Add: Function to get random upper case letters

--- Password_Generator/script.js
+++ Password_Generator/script.js
@@ -3,3 +3,10 @@ function getRandomLower() {
 }
 
 console.log(getRandomLower())
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) +65)
+}
+
+console.log(getRandomUpper())
+
