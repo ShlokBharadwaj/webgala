7cafefa11ba1fbfbdbbbaafd1d75e2fb8e2d1516
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-06-20 11:14:20 +0530

    Add: Squares

--- Hoverboard/script.js
+++ Hoverboard/script.js
@@ -1,3 +1,9 @@
 const container = document.getElementById('container')
 const colors = ["#03045e","023e8a","#0077b6","#0096c7","#00b4d8", "#48cae4", "#90e0ef", "#ade8f4", "#caf0f8"]
 const SQUARES = 500
+
+for(let i=0; i<SQUARES; i++) {
+    const squares = document.createElement('div')
+    squares.classList.add('square')
+    container.appendChild(squares)
+}
