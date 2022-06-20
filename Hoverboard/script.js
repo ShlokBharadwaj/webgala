5da13809e5c49ae1375664d9836fb85b78863ac3
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-06-20 11:13:49 +0530

    Add: Colors from coolors.co

---
+++ Hoverboard/script.js
@@ -0,0 +1,3 @@
+const container = document.getElementById('container')
+const colors = ["#03045e","023e8a","#0077b6","#0096c7","#00b4d8", "#48cae4", "#90e0ef", "#ade8f4", "#caf0f8"]
+const SQUARES = 500
