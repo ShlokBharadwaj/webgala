43d2427699c56e74a3b9f5eeef73aef5f005e4a0
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-06-20 11:28:52 +0530

    Add: Color on hover and remove functionality

--- Hoverboard/script.js
+++ Hoverboard/script.js
@@ -3,7 +3,27 @@ const colors = ["#03045e","023e8a","#0077b6","#0096c7","#00b4d8", "#48cae4", "#9
 const SQUARES = 500
 
 for(let i=0; i<SQUARES; i++) {
-    const squares = document.createElement('div')
-    squares.classList.add('square')
-    container.appendChild(squares)
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
 }
