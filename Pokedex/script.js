a239e543c2d0da7310d8fd0f800daa89b3370b56
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-06-22 16:11:57 +0530

    Add: Fetch details from API

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -1,33 +1,53 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 5
+const pokemon_count = 150
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
 }
 
 const fetchPokemons = async () => {
-    for(let i = 1; i <= pokemon_count; i++) {
+    for (let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i)
     }
 }
 
-const getPokemon =  async (id) => {
+const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEle = document.createElement('div')
+    pokemonEle.classList.add('pokemon')
+
+    const pokemonInnerHTML = `
+            <div class="img-container">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
+            </div>
+            <div class="info">
+                <span class="number">#${pokemon.id}</span>
+                <h3 class="name">${pokemon.name}</h3>
+                <small class="type">Type: <span></span></small>
+            </div> 
+            `
+
+            pokemonEle.innerHTML = pokemonInnerHTML
+
+            poke_container.appendChild(pokemonEle)
 }
 
 fetchPokemons()
