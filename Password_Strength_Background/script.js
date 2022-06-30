2defc58d9684b6bde2413298d5e105ab070e290e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-06-30 18:53:50 +0530

    Add: Function to check Special Characters

--- Password_Strength_Background/script.js
+++ Password_Strength_Background/script.js
@@ -2,13 +2,14 @@ const password = document.getElementById('password')
 const background = document.getElementById('background')
 
 function SpclChar(str) {
-    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
-    return specialChars.test(str)
+    var count = (str.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g)).length
+    return count
 }
 
 password.addEventListener('input', (e) => {
     const input = e.target.value
     const length = input.length
-    const blurValue = 15 - length*1.5
+    const splChar = SpclChar(input)
+    const blurValue = 15 - (length*0.5 + splChar*1)
     background.style.filter = `blur(${blurValue}px)`
 })
