ed38339c679ad358f8328f57b01e5353f9f0dbde
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-06-30 17:01:20 +0530

    Add: Regular expression for Special characters

--- Password_Strength_Background/script.js
+++ Password_Strength_Background/script.js
@@ -1,6 +1,11 @@
 const password = document.getElementById('password')
 const background = document.getElementById('background')
 
+function SpclChar(str) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
+    
+}
+
 password.addEventListener('input', (e) => {
     const input = e.target.value
     const length = input.length
