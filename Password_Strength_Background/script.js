3f48366f8875e808b118c3306c60149ea07bcb1f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-06-30 18:59:03 +0530

    Updated: blur removing speed

--- Password_Strength_Background/script.js
+++ Password_Strength_Background/script.js
@@ -10,6 +10,6 @@ password.addEventListener('input', (e) => {
     const input = e.target.value
     const length = input.length
     const splChar = SpclChar(input)
-    const blurValue = 15 - (length*0.5 + splChar*1)
+    const blurValue = 15 - (length*0.6 + splChar*1)
     background.style.filter = `blur(${blurValue}px)`
 })
