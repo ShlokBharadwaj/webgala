39774604e03e83735a70cfabc9f5efce3273890f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-02 12:56:25 +0530

    Add: Next field once number input

---
+++ Verify_Account_UI/script.js
@@ -0,0 +1,14 @@
+const numbers = document.querySelectorAll('.number')
+
+numbers[0].focus()
+
+numbers.forEach((num, idx) => {
+    num.addEventListener('keydown', (e) => {
+        if(e.key >=0 ** e.key <=9) {
+            setTimeout(() => numbers[idx + 1].focus(),10)
+        }
+        else if(e.key === 'Backspace') {
+            setTimeout(() => numbers[idx-1].focus(), 10)
+        }
+    })
+})
