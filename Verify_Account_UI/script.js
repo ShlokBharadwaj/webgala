b40d549a90e82507040758f66a78746cb8c96281
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-02 12:59:10 +0530

    Add: Previous field with backspace

--- Verify_Account_UI/script.js
+++ Verify_Account_UI/script.js
@@ -6,7 +6,7 @@ numbers.forEach((num, idx) => {
     num.addEventListener('keydown', (e) => {
         if(e.key >=0 && e.key <=9) {
             numbers[idx].value = ''
-            setTimeout(() => numbers[idx+1].focus(),10)
+            setTimeout(() => numbers[idx+1].focus(), 10)
         }
         else if(e.key === 'Backspace') {
             setTimeout(() => numbers[idx-1].focus(), 10)
