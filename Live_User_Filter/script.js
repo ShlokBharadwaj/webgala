b7b50d974d79bac62e9ce010b8c8050daa730015
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-03 21:00:47 +0530

    Update: Swapped Location and Email

--- Live_User_Filter/script.js
+++ Live_User_Filter/script.js
@@ -19,8 +19,8 @@ async function getData() {
       <div class="user-info">
         <h4>${user.name.title} ${user.name.first} ${user.name.last}</h4>
         <p><strong>Age:</strong> ${user.dob.age}</p>
-        <p><strong>Email:</strong> ${user.email}</p>
         <p><strong>Location:</strong> ${user.location.city}, ${user.location.country} - ${user.location.postcode}</p>
+        <p><strong>Email:</strong> ${user.email}</p>
         <p><strong>Phone No.:</strong> ${user.phone}</p>
         <p><strong>Username:</strong> ${user.login.username}</p>
         <p><strong>Password:</strong> ${user.login.password}</p>
