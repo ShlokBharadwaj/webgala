c74ce105cfe22218a70f0c18d4e6f01f7328b355
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-04 10:41:25 +0530

    Add: Heart according to rating

--- Feedback_UI_Design/script.js
+++ Feedback_UI_Design/script.js
@@ -1,16 +1,51 @@
 const ratings = document.querySelectorAll('.rating')
 const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
+let myRating = 'Happy'
+const ratingsCont = document.querySelector('.ratings-container')
 
-panel.addEventListener('click', (e) => {
+ratingsCont.addEventListener('click', (e) => {
     if(e.target.parentNode.classList.contains('rating')) {
         removeActive()
         e.target.parentNode.classList.add('active')
+        myRating = e.target.nextElementSibling.innerHTML
+        // removeActive()
     }
 })
 
 function removeActive() {
     ratings.forEach(element => {
         element.classList.remove('active')
-    });
+    })
 }
+
+sendBtn.addEventListener('click', (e) => {
+    console.log(myRating)
+    if(myRating === 'Happy') {
+        panel.innerHTML = `
+        <i class="fa-solid fa-heart-circle-check"></i>
+        <strong<Thank You!</strong>
+        <br>
+        <strong>Feedback: ${myRating}</strong>
+        <p>We appreciate your feedback and we'll try to improve our customer support!</p>
+    `
+    }
+    if(myRating === 'Neutral') {
+        panel.innerHTML = `
+        <i class="fa-solid fa-heart"></i>
+        <strong<Thank You!</strong>
+        <br>
+        <strong>Feedback: ${myRating}</strong>
+        <p>We appreciate your feedback and we'll try to improve our customer support!</p>
+    `
+    }
+    if(myRating === 'Unhappy') {
+        panel.innerHTML = `
+        <i class="fa-solid fa-heart-crack"></i>
+        <strong<Thank You!</strong>
+        <br>
+        <strong>Feedback: ${myRating}</strong>
+        <p>We appreciate your feedback and we'll try to improve our customer support!</p>
+    `
+    }
+})
