a37aca25b5de1c72d9064524450569fe9617641f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-06 01:03:00 +0530

    Add: To open hamburger menu

--- Netflix_Navigation/script.js
+++ Netflix_Navigation/script.js
@@ -2,4 +2,6 @@ const open_btn = document.querySelector('.open-btn')
 const close_btn = document.querySelector('.close-btn')
 const nav = document.querySelectorAll('.nav')
 
-
+open_btn.addEventListener('click', () => {
+    nav.forEach(nav_ele => nav_ele.classList.add('visible'))
+})
