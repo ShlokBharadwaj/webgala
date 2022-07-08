5565ab3597809b6a83aa662f2e6a4cfcbaf88b18
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2022-07-08 11:01:59 +0530

    Add: Variables to manipulate DOM

---
+++ Quiz_App/script.js
@@ -0,0 +1,9 @@
+const quiz = document.getElementById('quiz')
+const answerEle = document.querySelectorAll('answer')
+const questionsEle = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
