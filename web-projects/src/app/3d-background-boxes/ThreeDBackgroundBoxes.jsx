495d6abb02d5e5a189f243a1fd2f40abfbcb558c
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-05 09:36:51 +0530

    Update: Refactor event handling in ThreeDBackgroundBoxes component

---
+++ web-projects/src/app/3d-background-boxes/ThreeDBackgroundBoxes.jsx
@@ -0,0 +1,51 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import styles from './ThreeDBackgroundBoxes.module.css';
+
+const ThreeDBackgroundBoxes = () => {
+  useEffect(() => {
+    const boxesCont = document.getElementById('boxes');
+    const btn = document.getElementById('btn');
+
+    const createBoxes = () => {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+          const box = document.createElement('div');
+          box.classList.add(styles.box);
+          box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+          boxesCont.appendChild(box);
+        }
+      }
+    };
+
+    const handleButtonClick = () => {
+      boxesCont.classList.toggle(styles.activez);
+      boxesCont.querySelectorAll(`.${styles.box}`).forEach((box) => {
+        box.style.transform = boxesCont.classList.contains(styles.activez)
+          ? 'rotateZ(360deg)'
+          : 'rotateZ(0)';
+      });
+    };
+
+    btn.addEventListener('click', handleButtonClick);
+
+    createBoxes();
+
+    // Cleanup function
+    return () => {
+      btn.removeEventListener('click', handleButtonClick);
+    };
+  }, []);
+
+  return (
+    <div className={styles.main}>
+      <button id="btn" className={styles.magic}>
+        🎇Magic🎆
+      </button>
+      <div className={`${styles.boxes}`} id="boxes"></div>
+    </div>
+  );
+};
+
+export default ThreeDBackgroundBoxes;
