14e6fa9c3a3d5a5d1707ddad68de7fc91c6c1f77
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-06 15:28:09 +0530

    Add: Animated Countdown jsx component

--- web-projects/src/app/animated-countdowns/AnimatedCountdowns.jsx
+++ web-projects/src/app/animated-countdowns/AnimatedCountdowns.jsx
@@ -1,11 +1,29 @@
 'use client';
 
-import React from 'react'
+import React, { useEffect } from 'react';
+import styles from './AnimatedCountdowns.module.css';
 
 const AnimatedCountdowns = () => {
-  return (
-    <div>AnimatedCountdowns</div>
-  )
+    return (
+        <div className='main'>
+            <div className="counter">
+                <div className="nums">
+                    <span className="in">3</span>
+                    <span>2</span>
+                    <span>1</span>
+                    <span>0</span>
+                </div>
+                <h4>Get Ready!</h4>
+            </div>
+            
+            <div className="final">
+                <h1>GO</h1>
+                <button id="replay">
+                    <span>Replay</span>
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default AnimatedCountdowns
