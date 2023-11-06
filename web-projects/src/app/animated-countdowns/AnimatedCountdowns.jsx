f4c309490d0538aff239b523699fd94ebe52582f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-07 00:47:48 +0530

    Update: JSX code to follow naming conventions of CSS module

--- web-projects/src/app/animated-countdowns/AnimatedCountdowns.jsx
+++ web-projects/src/app/animated-countdowns/AnimatedCountdowns.jsx
@@ -5,20 +5,20 @@ import styles from './AnimatedCountdowns.module.css';
 
 const AnimatedCountdowns = () => {
     return (
-        <div className='main'>
-            <div className="counter">
-                <div className="nums">
-                    <span className="in">3</span>
+        <div className={styles.main}>
+            <div className={styles.counter}>
+                <div className={styles.nums}>
+                    <span className={styles.in}>3</span>
                     <span>2</span>
                     <span>1</span>
                     <span>0</span>
                 </div>
-                <h4>Get Ready!</h4>
+                <h4 className={styles.h4}>Get Ready!</h4>
             </div>
-            
-            <div className="final">
+
+            <div className={styles.final}>
                 <h1>GO</h1>
-                <button id="replay">
+                <button id={styles.replay}>
                     <span>Replay</span>
                 </button>
             </div>
