12eda922a0136140ef10a9f10f38e4d836df59c2
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-10 01:31:11 +0530

    Add: jsx for Auto text effect

--- web-projects/src/app/auto-text-effect/AutoTextEffect.jsx
+++ web-projects/src/app/auto-text-effect/AutoTextEffect.jsx
@@ -1,11 +1,18 @@
 'use client';
 
-import React from 'react'
+import React, { useState } from 'react';
+import styles from './AutoTextEffect.module.css';
 
 const AutoTextEffect = () => {
-  return (
-    <div>AutoTextEffect</div>
-  )
+    return (
+        <div className={styles.main}>
+            <h1 className={styles.h1} id='text'></h1>
+            <div className={styles.speedCont}>
+                <label htmlFor='speed'>Speed:</label>
+                <input type="number" name='speed' id='speed' value={1} min={1} max={5} step={1} />
+            </div>
+        </div>
+    )
 }
 
-export default AutoTextEffect
+export default AutoTextEffect;
