a22ef093de385ba80e04a86467de11cfc80bec48
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-10 02:01:30 +0530

    Add: Use React State management for speed

--- web-projects/src/app/auto-text-effect/AutoTextEffect.jsx
+++ web-projects/src/app/auto-text-effect/AutoTextEffect.jsx
@@ -1,15 +1,50 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './AutoTextEffect.module.css';
 
 const AutoTextEffect = () => {
+
+    const textRef = useRef(null);
+    const speedInputRef = useRef(null);
+
+    const [text, setText] = useState("Shlok Bharadwaj")
+    const [idx, setIdx] = useState(1);
+    const [speed, setSpeed] = useState(300);
+
+    useEffect(() => {
+        writeText();
+
+        function writeText() {
+            textRef.current.innerText = text.slice(0, idx);
+            setIdx((prevIdx) => (prevIdx % text.length) + 1);
+            setTimeout(writeText, speed);
+        }
+
+        const handleSpeedChange = (e) => setSpeed(300 / e.target.value);
+
+        speedInputRef.current.addEventListener('input', handleSpeedChange);
+
+        return () => {
+            speedInputRef.current.removeEventListener('input', handleSpeedChange);
+        };
+    }, [idx, speed, text]);
+
     return (
         <div className={styles.main}>
-            <h1 className={styles.h1} id='text'></h1>
+            <h1 className={styles.h1} ref={textRef}></h1>
             <div className={styles.speedCont}>
                 <label htmlFor='speed'>Speed:</label>
-                <input className={styles.inputCont}  type="number" name='speed' id='speed' value={1} min={1} max={5} step={1} />
+                <input
+                    className={styles.inputCont}
+                    type="number"
+                    name='speed'
+                    id='speed'
+                    value={1}
+                    min={1}
+                    max={5}
+                    step={1}
+                    ref={speedInputRef} />
             </div>
         </div>
     )
