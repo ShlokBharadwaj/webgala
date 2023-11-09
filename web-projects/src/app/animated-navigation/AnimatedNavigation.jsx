09a88f103a9a0f4b48951a5cd1c1002342a50f35
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-09 12:55:33 +0530

    Add: JSX code for Animated Navigation

--- web-projects/src/app/animated-navigation/AnimatedNavigation.jsx
+++ web-projects/src/app/animated-navigation/AnimatedNavigation.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import styles from './AnimatedNavigation.module.css'
 
 const AnimatedNavigation = () => {
-  return (
-    <div>AnimatedNavigation</div>
-  )
+    return (
+        <div className={styles.main}>
+            <nav className={styles.active} id='nav'>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Works</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <button className={styles.icon} id="toggle">
+                    <div className={`${styles.line} ${styles.line1}`}></div>
+                    <div className={`${styles.line} ${styles.line2}`}></div>
+                </button>
+            </nav>
+        </div>
+    )
 }
 
-export default AnimatedNavigation
+export default AnimatedNavigation;
