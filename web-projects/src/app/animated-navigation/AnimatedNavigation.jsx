07fe65093b56d78a444f3f5d5b14107488422b6f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-09 16:03:34 +0530

    Add: Toggle functionality using React state in AnimatedNavigation component

--- web-projects/src/app/animated-navigation/AnimatedNavigation.jsx
+++ web-projects/src/app/animated-navigation/AnimatedNavigation.jsx
@@ -1,25 +1,31 @@
 'use client';
 
-import React from 'react';
-import styles from './AnimatedNavigation.module.css'
+import React, { useState } from 'react';
+import styles from './AnimatedNavigation.module.css';
 
 const AnimatedNavigation = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleToggleClick = () => {
+        setIsActive((prev) => !prev);
+    };
+
     return (
         <div className={styles.main}>
-            <nav className={`${styles.active} ${styles.nav}`} id='nav'>
+            <nav className={`${isActive ? styles.active : ''} ${styles.nav}`} id='nav'>
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Works</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
-                <button className={styles.icon} id="toggle">
+                <button className={styles.icon} id="toggle" onClick={handleToggleClick}>
                     <div className={`${styles.line} ${styles.line1}`}></div>
                     <div className={`${styles.line} ${styles.line2}`}></div>
                 </button>
             </nav>
         </div>
-    )
-}
+    );
+};
 
 export default AnimatedNavigation;
