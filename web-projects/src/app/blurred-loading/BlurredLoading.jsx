41692214be88586c64154de2f80cb6e448aa717e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-12 02:34:46 +0530

    Add: Blurred Loading Animation

--- web-projects/src/app/blurred-loading/BlurredLoading.jsx
+++ web-projects/src/app/blurred-loading/BlurredLoading.jsx
@@ -8,6 +8,27 @@ const BlurredLoading = () => {
   const loadTextRef = useRef(null);
   const bgRef = useRef(null);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setLoad((prevLoad) => {
+        const newLoad = prevLoad + 1;
+        if (newLoad > 99) {
+          clearInterval(intervalId);
+        }
+        loadTextRef.current.innerText = `${newLoad}%`;
+        loadTextRef.current.style.opacity = scale(newLoad, 0, 100, 1, 0);
+        bgRef.current.style.filter = `blur(${scale(newLoad, 0, 100, 100, 0)}px)`;
+        return newLoad;
+      });
+    }, 25);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+  };
+
   return (
     <div className={styles.main}>
       <section className={styles.bg} ref={bgRef}></section>
