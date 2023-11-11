c86a33ddbfe4a7553cbb11ad1df28a2f46374a4f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-12 02:04:01 +0530

    Add: Blurred Loading component

--- web-projects/src/app/blurred-loading/BlurredLoading.jsx
+++ web-projects/src/app/blurred-loading/BlurredLoading.jsx
@@ -1,12 +1,19 @@
-'use client';
+'use client'
 
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styles from './BlurredLoading.module.css';
 
 const BlurredLoading = () => {
+  const [load, setLoad] = useState(0);
+  const loadTextRef = useRef(null);
+  const bgRef = useRef(null);
+
   return (
-    <div>BlurredLoading</div>
-  )
-}
+    <div className={styles.main}>
+      <section className={styles.bg} ref={bgRef}></section>
+      <div className={styles.loadingText} ref={loadTextRef}>0%</div>
+    </div>
+  );
+};
 
 export default BlurredLoading;
