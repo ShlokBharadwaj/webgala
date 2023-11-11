45dfc924c1f02f58171e7993024276dd36e81469
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-11 22:31:10 +0530

    Add: Font Awesome Icons

--- web-projects/src/app/background-slider/BackgroundSlider.jsx
+++ web-projects/src/app/background-slider/BackgroundSlider.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import styles from './BackgroundSlider.module.css';
 
 const BackgroundSlider = () => {
   return (
@@ -6,4 +7,4 @@ const BackgroundSlider = () => {
   )
 }
 
-export default BackgroundSlider
+export default BackgroundSlider;
