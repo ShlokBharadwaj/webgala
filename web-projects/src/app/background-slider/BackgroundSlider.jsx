9a8f59786e5f43056e0dff4d7e7067d31fc6076e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-11 23:37:16 +0530

    Add: Implement Background Slider React component

--- web-projects/src/app/background-slider/BackgroundSlider.jsx
+++ web-projects/src/app/background-slider/BackgroundSlider.jsx
@@ -1,11 +1,29 @@
 'use client'
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './BackgroundSlider.module.css';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const BackgroundSlider = () => {
+
+    const [activeSlide, setActiveSlide] = useState(0);
+
+    const slides = [
+        'https://source.unsplash.com/random/415/?cars',
+        'https://source.unsplash.com/random/415/?nature',
+        'https://source.unsplash.com/random/415/?food',
+        'https://source.unsplash.com/random/415/?travel'
+    ];
+
+    const handleLeftClick = () => {
+        setActiveSlide((prevSlide) => (prevSlide - 1 + slides.length) % slides.length)
+    };
+
+    const handleRightClick = () => {
+        setActiveSlide((prevSlide) => (prevSlide + 1) % slides.length)
+    };
+
     return (
         <div className={`${styles.main}`}>
             <div className={`${styles.sliderCont}`}>
