4c74805fa71a899c8672292cea32dc31c02c152d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-13 10:21:22 +0530

    Add: Button ripple effect component

--- web-projects/src/app/button-ripple-effect/ButtonRippleEffect.jsx
+++ web-projects/src/app/button-ripple-effect/ButtonRippleEffect.jsx
@@ -1,12 +1,18 @@
 'use client';
 
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './ButtonRippleEffect.module.css';
 
 const ButtonRippleEffect = () => {
-  return (
-    <div>ButtonRippleEffect</div>
-  )
-}
+    const buttonRef = useRef(null);
+
+    return (
+        <div className={styles.main}>
+            <button className={`${styles.ripple} ${styles.button}`} ref={buttonRef}>
+                Click Me
+            </button>
+        </div>
+    );
+};
 
 export default ButtonRippleEffect;
