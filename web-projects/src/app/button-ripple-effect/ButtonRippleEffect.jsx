b8824d7c58679bbf53fdee8441bc4f783f3f009d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-13 11:05:20 +0530

    Add: Button ripple effect on Button click

--- web-projects/src/app/button-ripple-effect/ButtonRippleEffect.jsx
+++ web-projects/src/app/button-ripple-effect/ButtonRippleEffect.jsx
@@ -6,9 +6,29 @@ import styles from './ButtonRippleEffect.module.css';
 const ButtonRippleEffect = () => {
     const buttonRef = useRef(null);
 
+    const handleClick = (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+
+        const buttonTop = buttonRef.current.offsetTop;
+        const buttonLeft = buttonRef.current.offsetLeft;
+
+        const xInside = x - buttonLeft;
+        const yInside = y - buttonTop;
+
+        const circle = document.createElement('span');
+        circle.classList.add('circle');
+        circle.style.top = `${yInside}px`;
+        circle.style.left = `${xInside}px`;
+
+        buttonRef.current.appendChild(circle);
+
+        setTimeout(() => circle.remove(), 500);
+    };
+
     return (
         <div className={styles.main}>
-            <button className={`${styles.ripple} ${styles.button}`} ref={buttonRef}>
+            <button className={`${styles.ripple} ${styles.button}`} ref={buttonRef} onClick={handleClick}>
                 Click Me
             </button>
         </div>
