31cb0d28bd3d20947b090b437fed996cbba0aa44
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-14 03:32:37 +0530

    Add: Intial component setup for Content placeholder

--- web-projects/src/app/content-placeholder/ContentPlaceholder.jsx
+++ web-projects/src/app/content-placeholder/ContentPlaceholder.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState, useRef } from 'react';
+import styles from './ContentPlaceholder.module.css';
 
 const ContentPlaceholder = () => {
-  return (
-    <div>ContentPlaceholder</div>
-  )
-}
+    const [dataLoaded, setDataLoaded] = useState(false);
+    const headerRef = useRef(null);
+    const titleRef = useRef(null);
+    const excerptRef = useRef(null);
+    const profileImgRef = useRef(null);
+    const nameRef = useRef(null);
+    const dateRef = useRef(null);
+
+    return (
+        <div className={`${styles.card} ${dataLoaded ? '' : styles.loading}`}>
+        </div>
+    );
+};
 
 export default ContentPlaceholder;
