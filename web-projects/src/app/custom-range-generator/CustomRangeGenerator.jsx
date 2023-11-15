baadf55c2e8f9799ffbba10f5a6386edb42eeead
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-16 00:26:05 +0530

    Add: Intial JSX structure for Custom range generator

--- web-projects/src/app/custom-range-generator/CustomRangeGenerator.jsx
+++ web-projects/src/app/custom-range-generator/CustomRangeGenerator.jsx
@@ -4,9 +4,14 @@ import React from 'react';
 import styles from './CustomRangeGenerator.module.css';
 
 const CustomRangeGenerator = () => {
-  return (
-    <div>CustomRangeGenerator</div>
-  )
+    return (
+        <div className={`${styles.main}`}>
+            <h2 className={`${styles.h2}`}>Slide for Custom Range:</h2>
+            <div className={`${rangeContainer}`}>
+
+            </div>
+        </div>
+    )
 }
 
 export default CustomRangeGenerator;
