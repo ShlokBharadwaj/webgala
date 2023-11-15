b7d58559e2c31814c908f8bc6a1945895566ca1d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-16 01:10:52 +0530

    Add: State and Event Handling for Range Input

--- web-projects/src/app/custom-range-generator/CustomRangeGenerator.jsx
+++ web-projects/src/app/custom-range-generator/CustomRangeGenerator.jsx
@@ -1,14 +1,21 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './CustomRangeGenerator.module.css';
 
 const CustomRangeGenerator = () => {
+
+    const [rangeValue, setRangeValue] = useState(50);
+
+    const handleRangeChange = (e) => {
+        setRangeValue(+e.target.value);
+    };
+
     return (
         <div className={`${styles.main}`}>
             <h2 className={`${styles.h2}`}>Slide for Custom Range:</h2>
-            <div className={`${rangeContainer}`}>
-
+            <div className={`${styles.rangeContainer}`}>
+                <input type="range" min={0} max={100} value={rangeValue} onChange={handleRangeChange} />
             </div>
         </div>
     )
