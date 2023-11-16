78d2528b0f6dbdee980e544285d129fa68f31749
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-17 00:38:17 +0530

    Add: Dad Jokes JSX component

--- web-projects/src/app/dad-jokes/DadJokes.jsx
+++ web-projects/src/app/dad-jokes/DadJokes.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+'use client';
+
+import React, { useRef } from 'react';
+import styles from './DadJokes.module.css';
 
 const DadJokes = () => {
+
+  const jokeRef = useRef(null);
+  const jokesBtnRef = useRef(null);
+
   return (
-    <div>DadJokes</div>
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <h3 className={styles.h3}>Don't Laugh Challenege</h3>
+        <div className={styles.joke} ref={jokeRef}>
+          // Here lies jokes...
+        </div>
+        <button className={styles.button} ref={jokesBtnRef}>Get another Joke</button>
+      </div>
+    </div>
   )
 }
 
