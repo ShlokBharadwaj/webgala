9e1babe374d8d56d2a1537382a3f36ff6eef4494
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-17 01:13:23 +0530

    Add: Fetch and display jokes initially and upon clicking the button

--- web-projects/src/app/dad-jokes/DadJokes.jsx
+++ web-projects/src/app/dad-jokes/DadJokes.jsx
@@ -1,24 +1,48 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './DadJokes.module.css';
 
 const DadJokes = () => {
+  const jokeRef = useRef();
+  const jokesBtnRef = useRef();
 
-  const jokeRef = useRef(null);
-  const jokesBtnRef = useRef(null);
+  const getJokes = async () => {
+    const config = {
+      headers: {
+        Accept: 'application/json',
+      },
+    };
+    const res = await fetch('https://icanhazdadjoke.com', config);
+    const data = await res.json();
+    console.log(data);
+    jokeRef.current.innerHTML = data.joke;
+  };
+
+  useEffect(() => {
+
+    getJokes();
+    jokesBtnRef.current.addEventListener('click', getJokes);
+
+    return () => {
+      jokesBtnRef.current.removeEventListener('click', getJokes);
+    };
+  }, []);
+
 
   return (
     <div className={styles.main}>
       <div className={styles.container}>
-        <h3 className={styles.h3}>Don't Laugh Challenege</h3>
+        <h3 className={styles.h3}>Don't Laugh Challenge</h3>
         <div className={styles.joke} ref={jokeRef}>
-          // Here lies jokes...
+          loading jokes...
         </div>
-        <button className={styles.button} ref={jokesBtnRef}>Get another Joke</button>
+        <button className={styles.button} ref={jokesBtnRef}>
+          Get another Joke
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default DadJokes;
