00598df48a619dad9088dbeca9cc4555893de423
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-18 00:02:51 +0530

    Add: Double heart click route

---
+++ web-projects/src/app/double-heart-click/DoubleHeartClick.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './DoubleHeartClick.module.css';
+
+const DoubleHeartClick = () => {
+  return (
+    <div>DoubleHeartClick</div>
+  )
+}
+
+export default DoubleHeartClick;
