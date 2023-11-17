90e050bae778bd5cb79a7b9bca0a6012cab2ca21
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-18 00:15:38 +0530

    Add: Double Heart Click JSX structure

--- web-projects/src/app/double-heart-click/DoubleHeartClick.jsx
+++ web-projects/src/app/double-heart-click/DoubleHeartClick.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useRef } from 'react';
 import styles from './DoubleHeartClick.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const DoubleHeartClick = () => {
-  return (
-    <div>DoubleHeartClick</div>
-  )
+
+    const timesRef = useRef(0);
+
+    return (
+        <div className={styles.main}>
+            <h3 className={styles.h3}>Double click on the Image to
+                <FontAwesomeIcon icon={faHeart} className={styles.faHeart} />
+                it!
+            </h3>
+            <small className={styles.small}>You liked it <span ref={timesRef}>0</span> times</small>
+            <div className={styles.loveMe}></div>
+        </div>
+    )
 }
 
 export default DoubleHeartClick;
