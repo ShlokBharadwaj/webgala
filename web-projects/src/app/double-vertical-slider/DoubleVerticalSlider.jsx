29ffc9e61d81b29730d6ba4637573296ef05a913
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-19 08:13:51 +0530

    Add: Color, Title and description for the left section

--- web-projects/src/app/double-vertical-slider/DoubleVerticalSlider.jsx
+++ web-projects/src/app/double-vertical-slider/DoubleVerticalSlider.jsx
@@ -5,7 +5,26 @@ import styles from './DoubleVerticalSlider.module.css';
 
 const DoubleVerticalSlider = () => {
   return (
-    <div>DoubleVerticalSlider</div>
+    <div className={styles.container}>
+        <div className={styles.leftSide}>
+            <div className='bg-[#c08009]'>
+                <h1>Dawn</h1>
+                <p>in all of its shades</p>
+            </div>
+            <div className='bg-[#905a02e2]'>
+                <h1>Infinite Sea</h1>
+                <p>with its endless possibilities</p>
+            </div>
+            <div className='bg-[#046c009d]'>
+                <h1>Alone</h1>
+                <p>in the wilderness</p>
+            </div>
+            <div className='bg-[#1b88b78c]'>
+                <h1>Shutter Island</h1>
+                <p>in the ocean</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
