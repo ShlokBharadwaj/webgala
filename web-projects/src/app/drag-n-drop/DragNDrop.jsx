f3b8688586447321498b030e517cc6debc396ae8
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-21 02:55:23 +0530

    Add: Map to iterate over empty divs, drag and drop functionality

--- web-projects/src/app/drag-n-drop/DragNDrop.jsx
+++ web-projects/src/app/drag-n-drop/DragNDrop.jsx
@@ -1,21 +1,51 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './DragNDrop.module.css';
 
 const DragNDrop = () => {
   const fillRef = useRef(null);
-  const emptiesRef = useRef(null);
+  const emptiesRefs = Array.from({ length: 4 }, () => useRef(null));
+  const [currentDropTarget, setCurrentDropTarget] = useState(null);
+
+  const handleDragStart = (e) => {
+    e.dataTransfer.setData('text/plain', '');
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const empty = e.target;
+
+    empty.classList.add(styles.fill);
+
+    if (currentDropTarget && currentDropTarget !== empty) {
+      currentDropTarget.classList.remove(styles.fill);
+    }
+
+    setCurrentDropTarget(empty);
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.empty} useRef={emptiesRef}>
-        <div className={styles.fill} draggable="true" useRef={fillRef}></div>
-      </div>
-      <div className={styles.empty} useRef={emptiesRef}></div>
-      <div className={styles.empty} useRef={emptiesRef}></div>
-      <div className={styles.empty} useRef={emptiesRef}></div>
-      <div className={styles.empty} useRef={emptiesRef}></div>
+      {emptiesRefs.map((emptyRef, index) => (
+        <div
+          key={index}
+          className={`${styles.empty} ${currentDropTarget === emptyRef.current ? styles.hovered : ''}`}
+          ref={emptyRef}
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+        ></div>
+      ))}
+      <div
+        className={styles.fill}
+        draggable="true"
+        onDragStart={handleDragStart}
+        ref={fillRef}
+      ></div>
     </div>
   );
 };
