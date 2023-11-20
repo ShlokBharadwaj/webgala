50a85e28589f87c6ba13f4a520ed815903c186c5
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-20 13:34:22 +0530

    Update: Use React State to avoid directly manipulating DOM

--- web-projects/src/app/drag-n-drop/DragNDrop.jsx
+++ web-projects/src/app/drag-n-drop/DragNDrop.jsx
@@ -1,19 +1,19 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './DragNDrop.module.css';
 
 const DragNDrop = () => {
-
+  const [parent, setParent] = useState(null);
   const fillRef = useRef(null);
 
   const dragStart = () => {
-    fillRef.current.classList.add('styles.hold');
+    fillRef.current.classList.add(styles.hold);
     setTimeout(() => (fillRef.current.className = 'invisible'), 0);
   };
 
   const dragEnd = () => {
-    fillRef.current.className = 'fill';
+    fillRef.current.className = styles.fill;
   };
 
   const dragOver = (e) => {
@@ -21,35 +21,65 @@ const DragNDrop = () => {
   };
 
   const dragEnter = () => {
-    fillRef.current.classList.add('styles.hovered');
+    fillRef.current.classList.add(styles.hovered);
   };
 
   const dragLeave = () => {
-    fillRef.current.className = 'empty';
+    fillRef.current.className = styles.empty;
   };
 
   const dragDrop = () => {
-    fillRef.current.className = 'empty';
-    fillRef.current.parentNode.appendChild(fillRef.current);
+    setParent(fillRef.current.parentNode);
+    fillRef.current.className = styles.empty;
   };
 
   return (
     <div className={styles.container}>
-      <div className={`${styles.empty} ${styles.draggable}`}
+      <div
+        className={`${styles.empty} ${styles.draggable}`}
         onDragOver={dragOver}
         onDragEnter={dragEnter}
         onDragLeave={dragLeave}
         onDrop={dragDrop}
       >
-        <div className={styles.fill} draggable="true" ref={fillRef} onDragStart={dragStart} onDragEnd={dragEnd}>
-        </div>
+        <div
+          className={styles.fill}
+          draggable="true"
+          ref={fillRef}
+          onDragStart={dragStart}
+          onDragEnd={dragEnd}
+        ></div>
       </div>
-      <div className={styles.empty} onDragOver={dragOver} onDragEnter={dragEnter} onDragLeave={dragLeave} onDrop={dragDrop}></div>
-      <div className={styles.empty} onDragOver={dragOver} onDragEnter={dragEnter} onDragLeave={dragLeave} onDrop={dragDrop}></div>
-      <div className={styles.empty} onDragOver={dragOver} onDragEnter={dragEnter} onDragLeave={dragLeave} onDrop={dragDrop}></div>
-      <div className={styles.empty} onDragOver={dragOver} onDragEnter={dragEnter} onDragLeave={dragLeave} onDrop={dragDrop}></div>
+      <div
+        className={styles.empty}
+        onDragOver={dragOver}
+        onDragEnter={dragEnter}
+        onDragLeave={dragLeave}
+        onDrop={dragDrop}
+      ></div>
+      <div
+        className={styles.empty}
+        onDragOver={dragOver}
+        onDragEnter={dragEnter}
+        onDragLeave={dragLeave}
+        onDrop={dragDrop}
+      ></div>
+      <div
+        className={styles.empty}
+        onDragOver={dragOver}
+        onDragEnter={dragEnter}
+        onDragLeave={dragLeave}
+        onDrop={dragDrop}
+      ></div>
+      <div
+        className={styles.empty}
+        onDragOver={dragOver}
+        onDragEnter={dragEnter}
+        onDragLeave={dragLeave}
+        onDrop={dragDrop}
+      ></div>
     </div>
-  )
-}
+  );
+};
 
 export default DragNDrop;
