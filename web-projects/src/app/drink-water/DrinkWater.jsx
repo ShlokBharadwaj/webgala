d22246bed0a61114c847ccdf265feeac36d98f62
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-22 23:56:42 +0530

    Fix: Bug where Big cup didn't get full

--- web-projects/src/app/drink-water/DrinkWater.jsx
+++ web-projects/src/app/drink-water/DrinkWater.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import styles from './DrinkWater.module.css';
 
 const DrinkWater = () => {
@@ -10,18 +10,23 @@ const DrinkWater = () => {
   const totalCups = 8;
   const [cups, setCups] = useState(Array(totalCups).fill(false));
 
-  const highlightCups = (idx) => {
-    setCups((prevCups) => prevCups.map((cup, cupIndex) => cupIndex <= idx));
+  useEffect(() => {
     updateBigCups();
+  }, [cups]);
+
+  const highlightCups = (idx) => {
+    const newCups = cups.map((cup, cupIndex) => cupIndex <= idx);
+    setCups(newCups);
   };
 
   const updateBigCups = () => {
     const fullCups = cups.filter((cup) => cup).length;
-    const percentageHeight = (fullCups / totalCups) * 330;
-    const litersText = `${2 - (250 * fullCups) / 1000}L`;
+    const percentageHeight = (fullCups / totalCups) * 100;
 
-    percentageRef.current.style.height = `${percentageHeight}px`;
-    percentageRef.current.innerText = `${(fullCups / totalCups) * 100}%`;
+    const clampedPercentage = Math.min(percentageHeight, 100);
+
+    percentageRef.current.style.height = `${clampedPercentage}%`;
+    percentageRef.current.innerText = `${clampedPercentage.toFixed(2)}%`;
 
     if (fullCups === 0) {
       percentageRef.current.style.visibility = 'hidden';
@@ -31,9 +36,10 @@ const DrinkWater = () => {
 
     if (fullCups === totalCups) {
       remainedRef.current.style.visibility = 'hidden';
+      remainedRef.current.style.height = 0;
     } else {
       remainedRef.current.style.visibility = 'visible';
-      litersRef.current.innerText = litersText;
+      litersRef.current.innerText = `${(2 - (250 * fullCups) / 1000).toFixed(2)}L Remains`;
     }
   };
 
@@ -66,3 +72,4 @@ const DrinkWater = () => {
 };
 
 export default DrinkWater;
+
