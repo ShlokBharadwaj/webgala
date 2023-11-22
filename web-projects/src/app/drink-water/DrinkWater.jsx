d76266edf5842ce21241ef8d12983cf0cfd15a48
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-22 21:58:57 +0530

    Update: Array and map logic

--- web-projects/src/app/drink-water/DrinkWater.jsx
+++ web-projects/src/app/drink-water/DrinkWater.jsx
@@ -8,7 +8,7 @@ const DrinkWater = () => {
   const remainedRef = useRef(null);
   const litersRef = useRef(null);
   const percentageRef = useRef(null);
-  const smallCupRef = Array(8).fill().map((_, i) => useRef(null));
+  const smallCupRefs = Array.from({ length: 8 }, () => useRef(null));
 
   return (
     <div className={styles.container}>
@@ -25,20 +25,13 @@ const DrinkWater = () => {
       <p className={styles.text}>Select how many glassed you have drank:</p>
 
       <div className={styles.cups}>
-        {/* <div className={`${styles.cup} ${styles.cupSmall}`} ref={smallCupRef}>250ml</div>
-        <div className={`${styles.cup} ${styles.cupSmall}`} ref={smallCupRef}>250ml</div>
-        <div className={`${styles.cup} ${styles.cupSmall}`} ref={smallCupRef}>250ml</div>
-        <div className={`${styles.cup} ${styles.cupSmall}`} ref={smallCupRef}>250ml</div>
-        <div className={`${styles.cup} ${styles.cupSmall}`} ref={smallCupRef}>250ml</div>
-        <div className={`${styles.cup} ${styles.cupSmall}`} ref={smallCupRef}>250ml</div>
-        <div className={`${styles.cup} ${styles.cupSmall}`} ref={smallCupRef}>250ml</div>
-        <div className={`${styles.cup} ${styles.cupSmall}`} ref={smallCupRef}>250ml</div> */}
         {
-          Array(8).fill().map((_, i) => {
-            return (
-              <div className={`${styles.cup} ${styles.cupSmall}`} key={i} ref={smallCupRef[i]}>250ml</div>
-            )
-          })
+          smallCupRefs.map((cupRef, index) => <div
+            key={index}
+            className={`${styles.cup} ${styles.cupSmall}`}
+            ref={cupRef}
+          >250ml</div>
+          )
         }
       </div>
     </div>
