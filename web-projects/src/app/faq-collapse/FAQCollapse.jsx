924edf6c292698198b2b8bb5aab7d5d81431d4b1
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-24 00:15:36 +0530

    Add: FAQ Collapse route

---
+++ web-projects/src/app/faq-collapse/FAQCollapse.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './FAQCollapse.module.css';
+
+const FAQCollapse = () => {
+  return (
+    <div>FAQCollapse</div>
+  )
+}
+
+export default FAQCollapse;
