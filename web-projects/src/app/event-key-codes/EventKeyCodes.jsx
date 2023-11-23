1f24cb9bfd11a34380604d464eb3c106502776ef
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-23 11:50:18 +0530

    Add: Event Key, Key code and code

--- web-projects/src/app/event-key-codes/EventKeyCodes.jsx
+++ web-projects/src/app/event-key-codes/EventKeyCodes.jsx
@@ -1,19 +1,50 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styles from './EventKeyCodes.module.css';
 
 const EventKeyCodes = () => {
-
     const insertRef = useRef(null);
-    
+    const [keyInfo, setKeyInfo] = useState({
+        key: '',
+        keyCode: '',
+        code: '',
+    });
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            setKeyInfo({
+                key: event.key === ' ' ? 'Space' : event.key,
+                keyCode: event.keyCode,
+                code: event.code,
+            });
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
     return (
         <div className={styles.container}>
             <div ref={insertRef}>
                 <div className={styles.key}>
-                    Press any key to get the KeyCode
+                    {keyInfo.key}
+                    <small>event.key</small>
+                </div>
+
+                <div className={styles.key}>
+                    {keyInfo.keyCode}
+                    <small>event.keyCode</small>
+                </div>
+
+                <div className={styles.key}>
+                    {keyInfo.code}
+                    <small>event.code</small>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default EventKeyCodes;
