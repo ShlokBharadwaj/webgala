cd7dc8b002e62869e349c9bddcfc9105eaf4d7b9
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-24 01:40:06 +0530

    Add: Dynamic render FAQs

--- web-projects/src/app/faq-collapse/FAQCollapse.jsx
+++ web-projects/src/app/faq-collapse/FAQCollapse.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FAQCollapse.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faMessage, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const FAQCollapse = () => {
-  return (
-    <div>FAQCollapse</div>
-  )
+
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const handleToggleClick = (index) => {
+        setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+    };
+
+    return (
+        <div className={styles.container}>
+            <h1>Frequently asked Questions</h1>
+            <div className={styles.faqContainer}>
+                {[1, 2, 3, 4, 5].map((index) => (
+                    <div key={index} className={`${styles.faq} ${activeIndex === index ? 'styles.active' : ''}`}>
+                        <h3 className={styles.faqTitle}>
+                            Lorem ipsum dolor sit amet?
+                        </h3>
+
+                        <p className={styles.faqText}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure repellendus sed officia doloremque? Hic, eveniet doloribus natus ullam officia provident?
+                        </p>
+                        <button className={styles.faqToggle} onClick={() => handleToggleClick(index)}>
+                            <FontAwesomeIcon icon={faChevronDown} className={`${activeIndex === index ? '' : styles.hidden}`} />
+                            <FontAwesomeIcon icon={faTimes} className={`${activeIndex === index ? '' : styles.hidden}`} />
+                            <FontAwesomeIcon icon={faMessage} className={`${activeIndex === index ? '' : styles.hidden}`} />
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default FAQCollapse;
