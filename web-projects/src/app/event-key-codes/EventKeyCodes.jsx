55807d83fc02946f548de09396f58d18f04ed5ca
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-23 10:48:33 +0530

    Add: JSX Structure for Event Key Codes

--- web-projects/src/app/event-key-codes/EventKeyCodes.jsx
+++ web-projects/src/app/event-key-codes/EventKeyCodes.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './EventKeyCodes.module.css';
 
 const EventKeyCodes = () => {
-  return (
-    <div>EventKeyCodes</div>
-  )
+
+    const insertRef = useRef(null);
+    
+    return (
+        <div className={styles.container}>
+            <div ref={insertRef}>
+                <div className={styles.key}>
+                    Press any key to get the KeyCode
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default EventKeyCodes;
