ddd113597821ba6ae5a20f4fd934bf8ad7f33e8d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-23 10:23:54 +0530

    Add: Event Key Codes route

---
+++ web-projects/src/app/event-key-codes/EventKeyCodes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './EventKeyCodes.module.css';
+
+const EventKeyCodes = () => {
+  return (
+    <div>EventKeyCodes</div>
+  )
+}
+
+export default EventKeyCodes;
