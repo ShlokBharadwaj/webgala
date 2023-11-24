ab0881cde100f4bfdfc09f29423665cae307ccb1
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-25 00:16:05 +0530

    Add: JSX structure with gifs, buttons

--- web-projects/src/app/feedback-ui-design/FeedbackUIDesign.jsx
+++ web-projects/src/app/feedback-ui-design/FeedbackUIDesign.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './FeedbackUIDesign.module.css';
+import { faL } from '@fortawesome/free-solid-svg-icons';
 
 const FeedbackUIDesign = () => {
-  return (
-    <div>FeedbackUIDesign</div>
-  )
+
+    const panelRef = useRef(null);
+    const sendRef = useRef(false);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.panelContainer} ref={panelRef}>
+                <strong>How would you rate your <br />experience with us?</strong>
+                <div className={styles.ratingsContainer}>
+                    <div className={styles.rating}>
+                        <img src="https://media2.giphy.com/media/xYJhVe7tIy8KUZCwkW/giphy.gif?cid=ecf05e4708ony0jdekbsf7oa6wto0o8u8g8sr5qped9p8fkf&rid=giphy.gif&ct=s"
+                            alt="Unhappy" />
+                        <small>Unhappy</small>
+                    </div>
+                    <div className={styles.rating}>
+                        <img src="https://media1.giphy.com/media/j4l0mCdcTFRyY4Bc5s/giphy.gif?cid=ecf05e476g38ig2o1ygj8v5xkd0oo964pzj1p0dzowr4ie4q&rid=giphy.gif&ct=g"
+                            alt="Neutral" />
+                        <small>Neutral</small>
+                    </div>
+                    <div className={styles.rating}>
+                        <img src="https://media0.giphy.com/media/QWvra259h4LCvdJnxP/giphy.gif?cid=ecf05e477miawv5xxfdxax03lro8fzied4d9thwt55kmscix&rid=giphy.gif&ct=g"
+                            alt="Happy" />
+                        <small>Happy</small>
+                    </div>
+                </div>
+                <button className={styles.button} ref={sendRef}>Submit Response</button>
+            </div>
+        </div>
+    )
 }
 
 export default FeedbackUIDesign;
