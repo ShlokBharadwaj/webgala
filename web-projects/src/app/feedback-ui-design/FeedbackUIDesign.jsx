b93996aba8c58ac0e5a2784158fbff84d1f8ed38
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-25 10:29:40 +0530

    Update: Refactor FeedbackUIDesign component using hooks and dynamic rendering

--- web-projects/src/app/feedback-ui-design/FeedbackUIDesign.jsx
+++ web-projects/src/app/feedback-ui-design/FeedbackUIDesign.jsx
@@ -19,26 +19,33 @@ const FeedbackUIDesign = () => {
             <div className={styles.panelContainer} ref={panelRef}>
                 <strong>How would you rate your <br />experience with us?</strong>
                 <div className={styles.ratingsContainer}>
-                    <div className={styles.rating}>
-                        <img src="https://media2.giphy.com/media/xYJhVe7tIy8KUZCwkW/giphy.gif?cid=ecf05e4708ony0jdekbsf7oa6wto0o8u8g8sr5qped9p8fkf&rid=giphy.gif&ct=s"
-                            alt="Unhappy" />
-                        <small>Unhappy</small>
-                    </div>
-                    <div className={styles.rating}>
-                        <img src="https://media1.giphy.com/media/j4l0mCdcTFRyY4Bc5s/giphy.gif?cid=ecf05e476g38ig2o1ygj8v5xkd0oo964pzj1p0dzowr4ie4q&rid=giphy.gif&ct=g"
-                            alt="Neutral" />
-                        <small>Neutral</small>
-                    </div>
-                    <div className={styles.rating}>
-                        <img src="https://media0.giphy.com/media/QWvra259h4LCvdJnxP/giphy.gif?cid=ecf05e477miawv5xxfdxax03lro8fzied4d9thwt55kmscix&rid=giphy.gif&ct=g"
-                            alt="Happy" />
-                        <small>Happy</small>
-                    </div>
+                    {['Unhappy', 'Neutral', 'Happy'].map((rating, index) => (
+                        <div
+                            key={index}
+                            className={`${styles.rating} ${selectedRating === index ? styles.selected : ''}`}
+                            onClick={() => handleRatingClick(index)}
+                        >
+                            <img
+                                src={`https://media${index}.giphy.com/media/${getImageUrl(index)}`}
+                                alt={rating}
+                            />
+                            <small>{rating}</small>
+                        </div>
+                    ))}
                 </div>
-                <button className={styles.button} ref={sendRef}>Submit Response</button>
+                <button className={styles.button} onClick={handleSubmitResponse}>Submit Response</button>
             </div>
         </div>
-    )
-}
+    );
+};
+
+const getImageUrl = (index) => {
+    const imageUrls = [
+        'xYJhVe7tIy8KUZCwkW/giphy.gif?cid=ecf05e4708ony0jdekbsf7oa6wto0o8u8g8sr5qped9p8fkf&rid=giphy.gif&ct=s',
+        'j4l0mCdcTFRyY4Bc5s/giphy.gif?cid=ecf05e476g38ig2o1ygj8v5xkd0oo964pzj1p0dzowr4ie4q&rid=giphy.gif&ct=g',
+        'QWvra259h4LCvdJnxP/giphy.gif?cid=ecf05e477miawv5xxfdxax03lro8fzied4d9thwt55kmscix&rid=giphy.gif&ct=g',
+    ];
+    return imageUrls[index];
+};
 
 export default FeedbackUIDesign;
