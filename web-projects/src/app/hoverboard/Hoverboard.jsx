c2883ef13e901a1d66042abb3296574434d9411f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-01 00:27:14 +0530

    Add: Set and Remove color function

--- web-projects/src/app/hoverboard/Hoverboard.jsx
+++ web-projects/src/app/hoverboard/Hoverboard.jsx
@@ -7,6 +7,22 @@ const Hoverboard = () => {
     const colors = ["#9b2226", "#ae2012", "#bb3e03", "#ca6702", "#ee9b00", "#e9d8a6", "#94d2bd", "#0a9396", "#005f73", "#001219", "#ffba08", "#7400b8", "#55a630", "#ffea00"];
     const SQUARES = 500;
 
+    useEffect(() => {
+        const container = containerRef.current;
+
+        const setColor = (element) => {
+            const color = getRandomColor();
+            element.style.background = color;
+            element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+        };
+
+        const removeColor = (element) => {
+            element.style.background = "#1d1d1d";
+            element.style.boxShadow = `0 0 2px #000`
+        };
+    })
+
+
     return (
         <div className={styles.container} ref={containerRef}>
         </div>
