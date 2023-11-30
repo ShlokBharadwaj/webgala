731b0492513fbee63837c7fde803d20e08585e31
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-30 22:40:21 +0530

    Add: Search widget toggle

--- web-projects/src/app/hidden-search-widget/HiddenSearchWidget.jsx
+++ web-projects/src/app/hidden-search-widget/HiddenSearchWidget.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './HiddenSearchWidget.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const HiddenSearchWidget = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleButtonClick = () => {
+        setIsActive(!isActive);
+    };
+
     return (
         <div className={styles.container}>
-            <div className={styles.search}>
+            <div className={`${styles.search} ${isActive ? styles.active : ''}`}>
                 <input type="text" className={styles.input} placeholder='Search...' />
-                <button className={styles.btn}>
-                    <FontAwesomeIcon icon={faSearch} className={styles.icon} />
+                <button className={styles.btn} onClick={handleButtonClick}>
+                    <FontAwesomeIcon icon={faSearch} />
                 </button>
             </div>
         </div>
