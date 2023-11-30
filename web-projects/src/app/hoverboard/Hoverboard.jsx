1030f5b9f450f73a5230fa3e0fdb9b21734a402f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-01 00:15:47 +0530

    Add: Hoverboard route

---
+++ web-projects/src/app/hoverboard/Hoverboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Hoverboard.module.css';
+
+const Hoverboard = () => {
+  return (
+    <div className={styles.container}>
+        Hoverboard
+    </div>
+  )
+}
+
+export default Hoverboard;
