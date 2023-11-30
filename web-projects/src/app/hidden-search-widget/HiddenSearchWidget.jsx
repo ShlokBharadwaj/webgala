060ac0cbfd276cc5ec24b8460ddf51139342e18b
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-11-30 22:01:48 +0530

    Add: FontAwesome icon and JSX structure for Hidden search widget

--- web-projects/src/app/hidden-search-widget/HiddenSearchWidget.jsx
+++ web-projects/src/app/hidden-search-widget/HiddenSearchWidget.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './HiddenSearchWidget.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const HiddenSearchWidget = () => {
-  return (
-    <div>HiddenSearchWidget</div>
-  )
+    return (
+        <div className={styles.container}>
+            <div className={styles.search}>
+                <input type="text" className={styles.input} placeholder='Search...' />
+                <FontAwesomeIcon icon={faSearch} className={styles.btn} />
+            </div>
+        </div>
+    )
 }
 
 export default HiddenSearchWidget;
