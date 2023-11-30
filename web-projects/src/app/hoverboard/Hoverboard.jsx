e124fcbdc38eae48b936237b6c5bbc8a5492bbab
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-01 00:24:04 +0530

    Add: Colors array and total number of squares

--- web-projects/src/app/hoverboard/Hoverboard.jsx
+++ web-projects/src/app/hoverboard/Hoverboard.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './Hoverboard.module.css';
 
 const Hoverboard = () => {
-  return (
-    <div className={styles.container}>
-        Hoverboard
-    </div>
-  )
+
+    const containerRef = useRef(null);
+    const colors = ["#9b2226", "#ae2012", "#bb3e03", "#ca6702", "#ee9b00", "#e9d8a6", "#94d2bd", "#0a9396", "#005f73", "#001219", "#ffba08", "#7400b8", "#55a630", "#ffea00"];
+    const SQUARES = 500;
+
+    return (
+        <div className={styles.container} ref={containerRef}>
+        </div>
+    )
 }
 
 export default Hoverboard;
