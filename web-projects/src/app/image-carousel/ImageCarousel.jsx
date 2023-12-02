549486e6a5841e25f42866120d0f10510bd7cae3
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-02 05:55:11 +0530

    Add: Image Carousel route

---
+++ web-projects/src/app/image-carousel/ImageCarousel.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ImageCarousel.module.css';
+
+const ImageCarousel = () => {
+  return (
+    <div>ImageCarousel</div>
+  )
+}
+
+export default ImageCarousel;
