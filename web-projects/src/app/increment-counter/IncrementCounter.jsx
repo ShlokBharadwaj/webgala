8e67696ade89df7c80e5ca5924aba417bf22f7cb
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-03 06:47:03 +0530

    Add: Increment Counter route

---
+++ web-projects/src/app/increment-counter/IncrementCounter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './IncrementCounter.module.css';
+
+const IncrementCounter = () => {
+  return (
+    <div>IncrementCounter</div>
+  )
+}
+
+export default IncrementCounter;
