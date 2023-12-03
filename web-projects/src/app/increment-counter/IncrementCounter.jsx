e48e120f01f55116c7ccbcd516e28992b1db54e0
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-03 07:19:38 +0530

    Add: Map through array to create brands icons and data

--- web-projects/src/app/increment-counter/IncrementCounter.jsx
+++ web-projects/src/app/increment-counter/IncrementCounter.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './IncrementCounter.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter, faInstagram, faGithub, faReddit } from '@fortawesome/free-brands-svg-icons';
 
 const IncrementCounter = () => {
-  return (
-    <div>IncrementCounter</div>
-  )
-}
+
+    const counterData = [
+        { icon: faXTwitter, count: 32000, title: 'Followers' },
+        { icon: faInstagram, count: 120000, title: 'Followers' },
+        { icon: faGithub, count: 8000, title: 'Stars' },
+        { icon: faReddit, count: 6000, title: 'Subscribers' }
+    ];
+
+    const [counters, setCounters] = useState(counterData);
+
+
+    return (
+        <div className={styles.container}>
+            {counters.map((counter, index) => (
+                <div key={index} className={styles.counterItem}>
+                    <FontAwesomeIcon icon={counter.icon} />
+                    <div className={styles.counter} data-target={counter.count}>
+                        {counter.current || 0}
+                    </div>
+                    <span>{counter.title}</span>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default IncrementCounter;
