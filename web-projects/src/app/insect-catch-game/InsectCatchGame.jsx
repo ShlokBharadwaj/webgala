2a1b7ac2c7f2b1fef4408805d4e8420af53b996f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-04 11:53:15 +0530

    Add: Route for Catch the insect game

---
+++ web-projects/src/app/insect-catch-game/InsectCatchGame.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './InsectCatchGame.module.css';
+
+const InsectCatchGame = () => {
+  return (
+    <div>InsectCatchGame</div>
+  )
+}
+
+export default InsectCatchGame;
