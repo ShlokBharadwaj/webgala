f54b06fc43560c586618833d65167c83f2652bae
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-04 13:39:45 +0530

    Add: Screen 2 of the game

--- web-projects/src/app/insect-catch-game/InsectCatchGame.jsx
+++ web-projects/src/app/insect-catch-game/InsectCatchGame.jsx
@@ -4,10 +4,47 @@ import styles from './InsectCatchGame.module.css';
 const InsectCatchGame = () => {
     return (
         <div className={`${styles.container} h-[100vh] overflow-hidden m-0 box-border text-center`}>
-            <div className='screen flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-[#2b9348]'>
+
+            {/* Screen 1 - Play Game*/}
+
+            {/* <div className='screen flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-[#2b9348]'>
                 <h1 className='text-5xl my-4'>Catch the Insect</h1>
-                <button className='btn border-0 bg-white text-[#2b9348] py-4 px-5 cursor-pointer focus:outline-none hover:opacity-90'>Play Game</button>
+                <button className='btn border-0 bg-white text-[#2b9348] py-4 px-5 cursor-pointer focus:outline-none hover:opacity-90 mt-10'>Play Game</button>
+            </div> */}
+
+            {/* Screen 2  - Choose Insect*/}
+
+            <div className='screen flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-[#2b9348]'>
+                <h1>What's your favourite insect?</h1>
+                <ul className="insect-list flex flex-wrap justify-center list-none p-0 mt-10">
+                    <li className="m-3">
+                        <button
+                            className="choose-insect-btn bg-transparent border-2 w-[150px] h-[150px] active:bg-[#007f5f] hover:scale-110">
+                            <p>Mantis</p>
+                            <img src="https://images.unsplash.com/photo-1630942592547-d7d223e15dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                                alt="Mantis" className="w-[100px] h-[100px] object-contain" />
+                        </button>
+                    </li>
+                    <li className="m-3">
+                        <button
+                            className="choose-insect-btn bg-transparent border-2 w-[150px] h-[150px]  active:bg-[#007f5f] hover:scale-110">
+                            <p>Spider</p>
+                            <img src="https://images.unsplash.com/photo-1579776711405-534256abcbb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=471&q=80"
+                                alt="Spider" className="w-[100px] h-[100px] object-contain" />
+                        </button>
+                    </li>
+                    <li className="m-3">
+                        <button
+                            className="choose-insect-btn bg-transparent border-2 w-[150px] h-[150px]  active:bg-[#007f5f] hover:scale-110">
+                            <p>Cockroach</p>
+                            <img src="https://images.unsplash.com/photo-1527942213181-ddbaba2e1496?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=446&q=80"
+                                alt="Cockroach" className="w-[100px] h-[100px] object-contain" />
+                        </button>
+                    </li>
+                </ul>
             </div>
+
+
         </div>
     )
 }
