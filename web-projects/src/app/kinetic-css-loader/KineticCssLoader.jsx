355b1089ee20facf7a55d26f3a0da3a71e535e39
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-05 02:34:30 +0530

    Add: JSX structure for Kinetic CSS Loader

--- web-projects/src/app/kinetic-css-loader/KineticCssLoader.jsx
+++ web-projects/src/app/kinetic-css-loader/KineticCssLoader.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './KineticCssLoader.module.css';
 
 const KineticCssLoader = () => {
-  return (
-    <div>KineticCssLoader</div>
-  )
+    return (
+        <div className={styles.container}>
+            <div className={styles.kinetic}></div>
+        </div>
+    )
 }
 
 export default KineticCssLoader;
