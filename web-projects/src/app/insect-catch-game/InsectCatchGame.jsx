81157da7bdfea448514b6f02bd115c13609db947
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-04 13:43:32 +0530

    Add: Screen 3 of the game

--- web-projects/src/app/insect-catch-game/InsectCatchGame.jsx
+++ web-projects/src/app/insect-catch-game/InsectCatchGame.jsx
@@ -14,7 +14,7 @@ const InsectCatchGame = () => {
 
             {/* Screen 2  - Choose Insect*/}
 
-            <div className='screen flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-[#2b9348]'>
+            {/* <div className='screen flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-[#2b9348]'>
                 <h1>What's your favourite insect?</h1>
                 <ul className="insect-list flex flex-wrap justify-center list-none p-0 mt-10">
                     <li className="m-3">
@@ -42,8 +42,24 @@ const InsectCatchGame = () => {
                         </button>
                     </li>
                 </ul>
-            </div>
+            </div> */}
 
+            {/* Screen 3 - End Game */}
+            <div
+                className="screen game-container flex flex-col items-center justify-center h-[100vh] w-[100vw] bg-[#588157] relative">
+                <h3 id="time" className="absolute top-5 right-5">Time: 00:00</h3>
+                <h3 id="score" className="absolute top-5 left-5">Score: 0</h3>
+                <h5 id="message"
+                    className="message leading-6 bg-[rgba(0,0,0,0.5)] w-full p-5 z-[100] text-center opacity-0 text-white absolute top-0 left-[50%] translate-x-[50%] translate-y-[-150%] transition-transform">
+                    Are you annoyed yet? <br />
+                        You are playing an impossible game
+                </h5>
+
+                {/* <div className="insect flex items-center justify-center w-[100px] h-[100px] absolute translate-x-[-50%] translate-y-[-50%] scale-100 cursor-pointer transition-transform">
+                    <img src="https://images.unsplash.com/photo-1527942213181-ddbaba2e1496?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=446&q=80"
+                        alt="" className="w-[100px] h-[100px]" />
+                </div> */}
+            </div>
 
         </div>
     )
