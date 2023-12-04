5b21f6fb58fad67ed1345579f86710f078260927
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-05 02:32:25 +0530

    Add: Kinetic CSS Loader route

---
+++ web-projects/src/app/kinetic-css-loader/KineticCssLoader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './KineticCssLoader.module.css';
+
+const KineticCssLoader = () => {
+  return (
+    <div>KineticCssLoader</div>
+  )
+}
+
+export default KineticCssLoader;
