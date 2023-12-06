ec12c7641f3bbaad8176459498af32d9794a9d2d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-06 09:43:14 +0530

    Add: JSX structure for Live user filter

--- web-projects/src/app/live-user-filter/LiveUserFilter.jsx
+++ web-projects/src/app/live-user-filter/LiveUserFilter.jsx
@@ -3,7 +3,20 @@ import styles from './LiveUserFilter.module.css';
 
 const LiveUserFilter = () => {
   return (
-    <div>LiveUserFilter</div>
+    <div className={styles.parentContainer}>
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <h4 className={styles.title}>Live User Filter</h4>
+                <small className={styles.subtitle}>Search by name or location</small>
+                <input type="text" name='name' id='filter' placeholder='Search...' className='w-1/3 py-2 border-b-2 border-blue-500 outline-none text-black focus:border-red-500 focus:outline-none' autoFocus/>
+            </header>
+            <ul className={styles.userList}>
+                <li>
+                    <h3>Loading...</h3>
+                </li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
