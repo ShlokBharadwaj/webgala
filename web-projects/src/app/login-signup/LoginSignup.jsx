2759804650a6efa3f1b733846fb3d13e73a3a81e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-08 00:13:45 +0530

    Add: Sign in, Sign up container

--- web-projects/src/app/login-signup/LoginSignup.jsx
+++ web-projects/src/app/login-signup/LoginSignup.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from './LoginSignup.module.css';
 
-
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className={styles.container}>
+        <div className={styles.childContainer}>
+            <div className={styles.initialContainer}>
+                <div className={`${styles.box} ${styles.signIn}`}>
+                    <h2>Already have an Account?</h2>
+                    <button className={styles.signInBtn}>Sign in</button>
+                </div>
+                <div className={`${styles.box} ${styles.signUp}`}>
+                    <h2>Don't have an Account?</h2>
+                    <button className={styles.signUpBtn}>Sign up</button>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 export default LoginSignup;
