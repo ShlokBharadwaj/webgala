029eb429b6d2521976d412027f2cb8c659438df6
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-07 06:07:17 +0530

    Add: User not found filter

--- web-projects/src/app/live-user-filter/LiveUserFilter.jsx
+++ web-projects/src/app/live-user-filter/LiveUserFilter.jsx
@@ -5,6 +5,7 @@ const LiveUserFilter = () => {
     const [users, setUsers] = useState([]);
     const [filteredUsers, setFilteredUsers] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -13,8 +14,10 @@ const LiveUserFilter = () => {
                 const { results } = await res.json();
                 setUsers(results);
                 setFilteredUsers(results);
+                setLoading(false);
             } catch (error) {
                 console.error('Error fetching data:', error);
+                setLoading(false);
             }
         };
 
@@ -51,38 +54,44 @@ const LiveUserFilter = () => {
                     />
                 </header>
                 <ul className={styles.userList}>
-                    {filteredUsers.length > 0 ? (
-                        filteredUsers.map((user) => (
-                            <li key={user.login.uuid} className={styles.userListItem}>
-                                <img src={user.picture.large} alt={user.name.first} />
-                                <div className={styles.userInfo}>
-                                    <h4>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h4>
-                                    <p>
-                                        <strong>Age:</strong> {user.dob.age}
-                                    </p>
-                                    <p>
-                                        <strong>Location:</strong> {`${user.location.city}, ${user.location.country} - ${user.location.postcode}`}
-                                    </p>
-                                    <p>
-                                        <strong>Email:</strong> {user.email}
-                                    </p>
-                                    <p>
-                                        <strong>Phone No.:</strong> {user.phone}
-                                    </p>
-                                    <p>
-                                        <strong>Username:</strong> {user.login.username}
-                                    </p>
-                                    <p>
-                                        <strong>Password:</strong> {user.login.password}
-                                    </p>
-                                </div>
-                            </li>
-                        ))
-                    ) : (
+                    {loading ? (
                         <li>
                             <h3>Loading...</h3>
                         </li>
-                    )}
+                    ) :
+                        filteredUsers.length > 0 ? (
+                            filteredUsers.map((user) => (
+                                <li key={user.login.uuid} className={styles.userListItem}>
+                                    <img src={user.picture.large} alt={user.name.first} />
+                                    <div className={styles.userInfo}>
+                                        <h4>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h4>
+                                        <p>
+                                            <strong>Age:</strong> {user.dob.age}
+                                        </p>
+                                        <p>
+                                            <strong>Location:</strong> {`${user.location.city}, ${user.location.country} - ${user.location.postcode}`}
+                                        </p>
+                                        <p>
+                                            <strong>Email:</strong> {user.email}
+                                        </p>
+                                        <p>
+                                            <strong>Phone No.:</strong> {user.phone}
+                                        </p>
+                                        <p>
+                                            <strong>Username:</strong> {user.login.username}
+                                        </p>
+                                        <p>
+                                            <strong>Password:</strong> {user.login.password}
+                                        </p>
+                                    </div>
+                                </li>
+                            ))
+                        ) : (
+                            <li>
+                                <h3>User not found.</h3>
+                            </li>
+                        )
+                    }
                 </ul>
             </div>
         </div>
