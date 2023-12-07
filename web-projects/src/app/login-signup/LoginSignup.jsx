7217e7b5d2aaff1172df7cf4d1a34fbbae3a5493
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-08 00:28:20 +0530

    Add: State variable to switch b/w sign in and sign up

--- web-projects/src/app/login-signup/LoginSignup.jsx
+++ web-projects/src/app/login-signup/LoginSignup.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './LoginSignup.module.css';
 
 const LoginSignup = () => {
+
+    const [isSignInActive, setSignInActive] = useState(true);
+
+    const handleSignUpClick = () => {
+        setSignInActive(false);
+    };
+
+    const handleSignInClick = () => {
+        setSignInActive(true);
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.childContainer}>
                 <div className={styles.initialContainer}>
                     <div className={`${styles.box} ${styles.signIn}`}>
                         <h2>Already have an Account?</h2>
-                        <button className={styles.signInBtn}>Sign in</button>
+                        <button className={styles.signInBtn} onClick={handleSignInClick}>Sign in</button>
                     </div>
                     <div className={`${styles.box} ${styles.signUp}`}>
                         <h2>Don't have an Account?</h2>
-                        <button className={styles.signUpBtn}>Sign up</button>
+                        <button className={styles.signUpBtn} onClick={handleSignUpClick}>Sign up</button>
                     </div>
                 </div>
                 <div className={styles.fromContainer}>
-                    <div className={`${styles.form} ${styles.signInForm}`}>
+                    <div className={`${styles.form} ${styles.signInForm} ${isSignInActive ? styles.active : ''}`}>
                         <form>
                             <h3>Sign In</h3>
                             <input type="email" placeholder='Email Address' />
@@ -25,7 +36,7 @@ const LoginSignup = () => {
                             <a href="#" className={styles.forgot}>Forgot Password</a>
                         </form>
                     </div>
-                    <div className={`${styles.form} ${styles.signUpForm}`}>
+                    <div className={`${styles.form} ${styles.signUpForm} ${!isSignInActive ? styles.active : ''}`}>
                         <form>
                             <h3>Sign Up</h3>
                             <input type="text" placeholder='Full Name' />
