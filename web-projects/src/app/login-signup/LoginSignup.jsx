69a93e421edcc0b0d9a8d2fa4137aa9670e35276
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-08 00:04:41 +0530

    Add: Route for login and signup

---
+++ web-projects/src/app/login-signup/LoginSignup.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './LoginSignup.module.css';
+
+
+const LoginSignup = () => {
+  return (
+    <div>LoginSignup</div>
+  )
+}
+export default LoginSignup;
