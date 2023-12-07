566dd775eddfcd583eedadc6ac5cb4058d687496
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-08 00:19:01 +0530

    Add: JSX for Sign in container

--- web-projects/src/app/login-signup/LoginSignup.jsx
+++ web-projects/src/app/login-signup/LoginSignup.jsx
@@ -2,21 +2,32 @@ import React from 'react';
 import styles from './LoginSignup.module.css';
 
 const LoginSignup = () => {
-  return (
-    <div className={styles.container}>
-        <div className={styles.childContainer}>
-            <div className={styles.initialContainer}>
-                <div className={`${styles.box} ${styles.signIn}`}>
-                    <h2>Already have an Account?</h2>
-                    <button className={styles.signInBtn}>Sign in</button>
+    return (
+        <div className={styles.container}>
+            <div className={styles.childContainer}>
+                <div className={styles.initialContainer}>
+                    <div className={`${styles.box} ${styles.signIn}`}>
+                        <h2>Already have an Account?</h2>
+                        <button className={styles.signInBtn}>Sign in</button>
+                    </div>
+                    <div className={`${styles.box} ${styles.signUp}`}>
+                        <h2>Don't have an Account?</h2>
+                        <button className={styles.signUpBtn}>Sign up</button>
+                    </div>
                 </div>
-                <div className={`${styles.box} ${styles.signUp}`}>
-                    <h2>Don't have an Account?</h2>
-                    <button className={styles.signUpBtn}>Sign up</button>
+                <div className={styles.fromContainer}>
+                    <div className={`${styles.form} ${styles.signInForm}`}>
+                        <form>
+                            <h3>Sign In</h3>
+                            <input type="text" placeholder='Email Address' />
+                            <input type="password" placeholder='Password' />
+                            <input type="submit" value='Login' />
+                            <a href="#" className={styles.forgot}>Forgot Password</a>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 export default LoginSignup;
