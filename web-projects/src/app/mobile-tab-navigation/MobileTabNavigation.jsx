d7adefee32b89a1a9ef85351f59603ef41a0840f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-10 00:55:09 +0530

    Add: Mobile tab navigation route

---
+++ web-projects/src/app/mobile-tab-navigation/MobileTabNavigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './MobileTabNavigtion.module.css';
+
+const MobileTabNavigation = () => {
+  return (
+    <div>MobileTabNavigation</div>
+  )
+}
+
+export default MobileTabNavigation;
