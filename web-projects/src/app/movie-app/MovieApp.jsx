2938623c500a7de5223a93ecf1f9da7351a67075
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-11 05:52:46 +0530

    Add: JSX for Movie app

--- web-projects/src/app/movie-app/MovieApp.jsx
+++ web-projects/src/app/movie-app/MovieApp.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './MovieApp.module.css';
 
 const MovieApp = () => {
-  return (
-    <div>MovieApp</div>
-  )
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSubmit = (e) => {
+    }
+
+    const handleSearch = (e) => {
+    }
+
+    return (
+        <div className={styles.container}>
+            <header>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" className={styles.search} placeholder='Search...' value={searchTerm} onChange={handleSearch} />
+                </form>
+            </header>
+            <main></main>
+        </div>
+    )
 }
 
 export default MovieApp;
