3648ee02c4e83278d524f33740f01f7cf4109772
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-11 06:00:37 +0530

    Add: Search Movie via API

--- web-projects/src/app/movie-app/MovieApp.jsx
+++ web-projects/src/app/movie-app/MovieApp.jsx
@@ -6,11 +6,35 @@ const MovieApp = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
 
+    const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8f6a0fca12c89f86f46fe4da733ada3e&page=1';
+    const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+    const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=8f6a0fca12c89f86f46fe4da733ada3e&query=';
+
+    useEffect(() => {
+        getMovies(API_URL);
+    }, []);
+
+    const getMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setMovies(data.results);
+    };
+
     const handleSubmit = (e) => {
-    }
+        e.preventDefault();
+
+        if (searchTerm && searchTerm !== '') {
+            getMovies(SEARCH_API + searchTerm);
+            setSearchTerm('');
+        } else {
+            window.location.reload();
+        }
+    };
 
     const handleSearch = (e) => {
-    }
+        setSearchTerm(e.target.value);
+    };
 
     return (
         <div className={styles.container}>
