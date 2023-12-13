5f577539f226dfe3b7c1d7ecede9fae1d9a5ff3b
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-13 15:29:51 +0530

    Add: Icon to open side navbar

--- web-projects/src/app/netflix-navigation/NetflixNavigation.jsx
+++ web-projects/src/app/netflix-navigation/NetflixNavigation.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './NetflixNavigation.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const NetflixNavigation = () => {
-  return (
-    <div>NetflixNavigation</div>
-  )
+
+    const [isNavVisible, setNavVisibility] = useState(false);
+
+    const handleOpenClick = () => {
+        setNavVisibility(true);
+    }
+    return (
+        <div className={styles.container}>
+            <button className={`${styles.navBtn} ${styles.openBtn}`} onClick={handleOpenClick}>
+                <FontAwesomeIcon icon={faBars} />
+            </button>
+        </div>
+    )
 }
 
 export default NetflixNavigation;
