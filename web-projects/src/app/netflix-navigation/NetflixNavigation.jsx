cfddb45d7ba6fa0a686a26cad92b3f1638dc834f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-13 15:18:58 +0530

    Add: Route for Netflix navigation

---
+++ web-projects/src/app/netflix-navigation/NetflixNavigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './NetflixNavigation.module.css';
+
+const NetflixNavigation = () => {
+  return (
+    <div>NetflixNavigation</div>
+  )
+}
+
+export default NetflixNavigation;
---
+++ web-projects/src/app/netflix-navigation/page.js
@@ -0,0 +1,1 @@
+export { default } from './NetflixNavigation';
