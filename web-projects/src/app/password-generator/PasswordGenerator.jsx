b6ce1fe1933a33cf789b37b048e2571277117f07
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-18 16:11:57 +0530

    Add: Array to map through label

--- web-projects/src/app/password-generator/PasswordGenerator.jsx
+++ web-projects/src/app/password-generator/PasswordGenerator.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import styles from './PasswordGenerator.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClipboard } from '@fortawesome/free-regular-svg-icons';
 
 const PasswordGenerator = () => {
-  return (
-    <div className={styles.main}>
-        <div className={styles.contianer}>
-            <h2>Password Generator</h2>
+
+    const passItems = [
+        { id: 'length', label: 'Password Length', type: 'number', min: 8, max: 25, defaultValue: 20 },
+        { id: 'uppercase', label: 'Include uppercase letters', type: 'checkbox', defaultChecked: true },
+        { id: 'lowercase', label: 'Include lowercase letters', type: 'checkbox', defaultChecked: true },
+        { id: 'numbers', label: 'Include numbers', type: 'checkbox', defaultChecked: true },
+        { id: 'symbols', label: 'Include symbols', type: 'checkbox', defaultChecked: true },
+    ];
+
+    return (
+        <div className={styles.main}>
+            <div className={styles.contianer}>
+                <h2>Password Generator</h2>
+
+                <div className={styles.resultContainer}>
+                    <span id='result' />
+                    <button className={styles.btn} id='clipboard'>
+                        <FontAwesomeIcon icon={faClipboard} />
+                    </button>
+                </div>
+
+                <div className={styles.passConfigs}>
+                    {passItems.map((item) => (
+                        <div key={item.id} className={styles.config}>
+                            <label>{item.label}</label>
+                            {item.type === 'number' ? (
+                                <input type={item.type} id={item.id} min={item.min} max={item.max} defaultValue={item.defaultValue} />
+                            ) : (
+                                <input type={item.type} id={item.id} defaultChecked={item.defaultChecked} />
+                            )}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default PasswordGenerator;
