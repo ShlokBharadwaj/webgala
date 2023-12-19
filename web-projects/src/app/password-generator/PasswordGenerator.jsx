c6556796fa9c6fb5ea96818d7017417777fa52db
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-19 19:33:15 +0530

    Add: Functions to generate password combinations

--- web-projects/src/app/password-generator/PasswordGenerator.jsx
+++ web-projects/src/app/password-generator/PasswordGenerator.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './PasswordGenerator.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClipboard } from '@fortawesome/free-regular-svg-icons';
 
 const PasswordGenerator = () => {
+
+    const [password, setPassword] = useState('');
+    const [passwordLength, setPasswordLength] = useState(20);
+    const [includeUppercase, setIncludeUppercase] = useState(true);
+    const [includeLowercase, setIncludeLowercase] = useState(true);
+    const [includeNumbers, setIncludeNumbers] = useState(true);
+    const [includeSymbols, setIncludeSymbols] = useState(true);
+    const resultRef = useRef(null);
+
+    const randomFunc = {
+        lower: getRandomLower,
+        upper: getRandomUpper,
+        number: getRandomNumber,
+        symbol: getRandomSymbol,
+    };
 
     const passItems = [
         { id: 'length', label: 'Password Length', type: 'number', min: 8, max: 25, defaultValue: 20 },
@@ -41,7 +56,15 @@ const PasswordGenerator = () => {
                 <button className={`${styles.btn2} ${styles.btnLarge}`} id='generate'>Generate Password</button>
             </div>
         </div>
-    )
-}
+    );
+};
+
+const getRandomLower = () => String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+const getRandomUpper = () => String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+const getRandomNumber = () => String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+const getRandomSymbol = () => {
+    const symbols = '!@#$%^&*(){}[]=<>|/,.~`';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
 
 export default PasswordGenerator;
