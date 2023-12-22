3615d12b03f79d372ad4f00010d6ef77e0138a51
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-22 14:26:55 +0530

    Update: Use Tailwind over .css and add background with blur

--- web-projects/src/app/password-strength-background/PasswordStrengthBackground.jsx
+++ web-projects/src/app/password-strength-background/PasswordStrengthBackground.jsx
@@ -3,9 +3,13 @@ import styles from './PasswordStrengthBackground.module.css';
 
 const PasswordStrengthBackground = () => {
   return (
-    <div className={styles.main}>
-      <div className={styles.bg} id='bg'></div>
-      <div className='bg-white text-black rounded-md p-10 text-center shadow-md'>
+    <div className="box-border flex flex-col items-center justify-center min-h-[100vh] overflow-hidden m-0">
+      <div
+        className="bg-cover bg-no-repeat bg-center absolute -z-10 -top-0 -left-0 -right-0 -bottom-0 w-full h-full blur-lg"
+        style={{ backgroundImage: 'url("https://source.unsplash.com/random/?landscape")' }}
+      ></div>
+
+      <div className='bg-white text-black rounded-md p-10 text-center shadow-md mx-5'>
         <h1 className='text-3xl'>Image Strength Password</h1>
         <p className='text-sm text-gray-700 mt-2'>Enter long password to see the effect. <br />Hint: Use special characters to speed up the process.</p>
         <div className='my-8 text-left'>
@@ -16,7 +20,6 @@ const PasswordStrengthBackground = () => {
           <label htmlFor="password" className='text-gray-800'>Password:</label>
           <input type="password" className='border block w-full p-2 rounded-md' id='password' placeholder='Enter you Password...' />
         </div>
-
         <button className='bg-black text-white py-2 mt-4 inline-block w-1/2 rounded-md'>Submit</button>
       </div>
     </div>
