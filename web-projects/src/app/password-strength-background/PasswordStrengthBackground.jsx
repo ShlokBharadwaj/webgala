76680f0101d032e856e909137003eb99d47011ed
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-22 13:38:04 +0530

    Add: Pass Strength Bg form field w Tailwind

--- web-projects/src/app/password-strength-background/PasswordStrengthBackground.jsx
+++ web-projects/src/app/password-strength-background/PasswordStrengthBackground.jsx
@@ -3,8 +3,24 @@ import styles from './PasswordStrengthBackground.module.css';
 
 const PasswordStrengthBackground = () => {
   return (
-    <div>PasswordStrengthBackground</div>
-  )
-}
+    <div className={styles.main}>
+      <div className={styles.bg} id='bg'></div>
+      <div className='bg-white text-black rounded-md p-10 text-center shadow-md'>
+        <h1 className='text-3xl'>Image Strength Password</h1>
+        <p className='text-sm text-gray-700 mt-2'>Enter long password to see the effect. <br />Hint: Use special characters to speed up the process.</p>
+        <div className='my-8 text-left'>
+          <label htmlFor="email" className='text-gray-800'>Email:</label>
+          <input type="text" className='border block w-full p-2 rounded-md' id='email' placeholder='Enter you Email...' />
+        </div>
+        <div className='my-4 text-left'>
+          <label htmlFor="password" className='text-gray-800'>Password:</label>
+          <input type="password" className='border block w-full p-2 rounded-md' id='password' placeholder='Enter you Password...' />
+        </div>
+
+        <button className='bg-black text-white py-2 mt-4 inline-block w-1/2 rounded-md'>Submit</button>
+      </div>
+    </div>
+  );
+};
 
 export default PasswordStrengthBackground;
