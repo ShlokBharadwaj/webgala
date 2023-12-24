e1017801070f11de0ad48426f004d65bd09f5638
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-24 13:13:52 +0530

    Add: Color for all pokemon types

--- web-projects/src/app/pokedex/Pokedex.jsx
+++ web-projects/src/app/pokedex/Pokedex.jsx
@@ -7,6 +7,29 @@ const Pokedex = () => {
     const [loading, setLoading] = useState(true);
     const totalPokemonCount = 1025;
 
+    const colors = {
+        normal: '#A8A878',
+        fighting: '#C03028',
+        flyting: '#A890F0',
+        posion: '#A040A0',
+        ground: '#E0C068',
+        rock: '#B8A038',
+        bug: '#A8B820',
+        ghost: '#705898',
+        steel: '#B8B8D0',
+        fire: '#F08030',
+        water: '#6890F0',
+        grass: '#78C850',
+        electric: '#F8D030',
+        psychic: '#F85888',
+        ice: '#98D8D8',
+        dragon: '#7038F8',
+        dark: '#705848',
+        fairy: '#EE99AC',
+        unknown: '#CCCCCC',
+        shadow: '#333333',
+    };
+
     return (
         <div className={styles.container}>
             <h1>Pokedex</h1>
