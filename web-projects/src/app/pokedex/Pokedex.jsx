1d93fbb3ee995a7171cdb0dd1abb468ea32c6e05
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-24 10:20:41 +0530

    Add: State veriable and total count of Pokemon

--- web-projects/src/app/pokedex/Pokedex.jsx
+++ web-projects/src/app/pokedex/Pokedex.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Pokedex.module.css';
 
 const Pokedex = () => {
+
+    const [pokemonData, setPokemonData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const totalPokemonCount = 1025;
+
     return (
         <div className={styles.container}>
             <h1>Pokedex</h1>
