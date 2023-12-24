0fedafe6ee9435a536cb6f48726859cb7347bbd8
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-24 10:18:11 +0530

    Add: Initial JSX for Pokedex

--- web-projects/src/app/pokedex/Pokedex.jsx
+++ web-projects/src/app/pokedex/Pokedex.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import styles from './Pokedex.module.css';
 
 const Pokedex = () => {
-  return (
-    <div>Pokedex</div>
-  )
+    return (
+        <div className={styles.container}>
+            <h1>Pokedex</h1>
+            <small>Have nostalgia with all 1025 pokemons</small>
+            <div className={styles.pokeContainer} id="pokeContainer"></div>
+        </div>
+    )
 }
 
 export default Pokedex;
