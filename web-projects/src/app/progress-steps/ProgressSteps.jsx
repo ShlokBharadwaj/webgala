dd775bc4947b3cf3397acfd41e66ebcb1adca38d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-31 00:43:52 +0530

    Add: Route for Progress steps

---
+++ web-projects/src/app/progress-steps/ProgressSteps.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ProgressSteps.module.css';
+
+const ProgressSteps = () => {
+  return (
+    <div>ProgressSteps</div>
+  )
+}
+
+export default ProgressSteps;
