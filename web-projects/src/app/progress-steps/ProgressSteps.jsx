e88bf039b9b558c79a9b0a27327d9b913a44e8c9
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-01 10:21:09 +0530

    Add: JSX structure for Progress Steps

--- web-projects/src/app/progress-steps/ProgressSteps.jsx
+++ web-projects/src/app/progress-steps/ProgressSteps.jsx
@@ -3,7 +3,21 @@ import styles from './ProgressSteps.module.css';
 
 const ProgressSteps = () => {
   return (
-    <div>ProgressSteps</div>
+    <div className={styles.container}>
+      <div className={styles.childContainer}>
+        <div className={styles.progressContainer}>
+          <div className={styles.progress} id="progress">
+            <div className={`${styles.circle} ${styles.active}`}>1</div>
+            <div className={`${styles.circle}`}>2</div>
+            <div className={`${styles.circle}`}>3</div>
+            <div className={`${styles.circle}`}>4</div>
+          </div>
+
+          <button className={styles.btn} id="prev" disabled>Previous</button>
+          <button className={styles.btn} id="next">Next</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
