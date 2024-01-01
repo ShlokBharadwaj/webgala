afe587b0021c8905003ae257092e5ee66276983a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-01 10:34:07 +0530

    Add: Logic to change Steps and disable button

--- web-projects/src/app/progress-steps/ProgressSteps.jsx
+++ web-projects/src/app/progress-steps/ProgressSteps.jsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ProgressSteps.module.css';
 
 const ProgressSteps = () => {
+  const [currentActive, setCurrentActive] = useState(1);
+
+  const handleNextClick = () => {
+    setCurrentActive((prevActive) => Math.min(prevActive + 1, 4));
+  };
+
+  const handlePrevClick = () => {
+    setCurrentActive((prevActive) => Math.max(prevActive - 1, 1));
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.childContainer}>
         <div className={styles.progressContainer}>
-          <div className={styles.progress} id="progress">
-            <div className={`${styles.circle} ${styles.active}`}>1</div>
-            <div className={`${styles.circle}`}>2</div>
-            <div className={`${styles.circle}`}>3</div>
-            <div className={`${styles.circle}`}>4</div>
+          <div className={styles.progress}>
+            {[1, 2, 3, 4].map((step, index) => (
+              <div
+                key={index}
+                className={`${styles.circle} ${index < currentActive ? styles.active : ''
+                  }`}
+              >
+                {step}
+              </div>
+            ))}
           </div>
 
-          <button className={styles.btn} id="prev" disabled>Previous</button>
-          <button className={styles.btn} id="next">Next</button>
+          <button
+            className={styles.btn}
+            disabled={currentActive === 1}
+            onClick={handlePrevClick}
+          >
+            Previous
+          </button>
+          <button
+            className={styles.btn}
+            disabled={currentActive === 4}
+            onClick={handleNextClick}
+          >
+            Next
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProgressSteps;
