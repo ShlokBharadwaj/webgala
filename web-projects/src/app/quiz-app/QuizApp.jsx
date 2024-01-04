bfa09257b6bb9d663ee42009d60deed85fa1fb6a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-04 16:01:08 +0530

    Add: Static JSX strucutre for Quiz App

--- web-projects/src/app/quiz-app/QuizApp.jsx
+++ web-projects/src/app/quiz-app/QuizApp.jsx
@@ -3,7 +3,32 @@ import styles from './QuizApp.module.css';
 
 const QuizApp = () => {
     return (
-        <div>QuizApp</div>
+        <div className={styles.container}>
+            <div className={styles.quizContainer} id="quiz">
+                <div className={styles.quizHeader}>
+                    <h2 id="question">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, porro.</h2>
+                    <ul>
+                        <li>
+                            <input type="radio" name="answer" className={styles.answer} id="a" />
+                            <label htmlFor="a" id="a_text">Answer 1</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="answer" className={styles.answer} id="b" />
+                            <label htmlFor="b" id="b_text">Answer 2</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="answer" className={styles.answer} id="c" />
+                            <label htmlFor="c" id="c_text">Answer 3</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="answer" className={styles.answer} id="d" />
+                            <label htmlFor="d" id="d_text">Answer 4</label>
+                        </li>
+                    </ul>
+                </div>
+                <button id="submit">Submit</button>
+            </div>
+        </div>
     )
 }
 
