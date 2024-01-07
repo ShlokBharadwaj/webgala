2de771258a07c25aab1f810cedb66fa6f9de35ec
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-07 12:51:56 +0530

    Add: Space and Enter handling function

--- web-projects/src/app/random-choice-picker/RandomChoicePicker.jsx
+++ web-projects/src/app/random-choice-picker/RandomChoicePicker.jsx
@@ -8,6 +8,37 @@ const RandomChoicePicker = () => {
     const tagsContainerRef = useRef(null);
     const tagElementsRef = useRef([]);
 
+    useEffect(() => {
+        textareaRef.current.focus();
+        tagElementsRef.current = Array.from(tagsContainerRef.current.children);
+    }, []);
+
+    const handleKeyUp = (e) => {
+        if (e.key === 'Enter') {
+            handleEnter();
+        } else {
+            handleSpace(e.target.value);
+        }
+    };
+
+    const handleEnter = () => {
+        const newTags = textareaRef.current.value.split(' ').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+        setTags(newTags);
+
+        setTimeout(() => {
+            textareaRef.current.value = '';
+        }, 10);
+
+        randomSelect();
+    };
+
+    const handleSpace = (input) => {
+        const newTags = input.split(' ').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+        setTags(newTags);
+    };
+
+    const randomSelect = (input) => { };
+
     return (
         <div className={styles.container}>
             <div className={styles.choiceContainer}>
@@ -16,7 +47,8 @@ const RandomChoicePicker = () => {
                 </h3>
                 <textarea
                     placeholder="Enter your choices here..."
-                    ref={textareaRef}>
+                    ref={textareaRef}
+                    onKeyUp={handleKeyUp}>
                 </textarea>
                 <div ref={tagsContainerRef}>
                     {tags.map((tag, index) => (
