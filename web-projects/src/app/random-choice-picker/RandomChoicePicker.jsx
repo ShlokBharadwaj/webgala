0bd4e74b0c3a239197c0d8e69de0d65f5721b01f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-07 12:29:55 +0530

    Update: Replace IDs with Hooks

--- web-projects/src/app/random-choice-picker/RandomChoicePicker.jsx
+++ web-projects/src/app/random-choice-picker/RandomChoicePicker.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './RandomChoicePicker.module.css';
 
 const RandomChoicePicker = () => {
+
+    const [tags, setTags] = useState([]);
+    const textareaRef = useRef(null);
+    const tagsContainerRef = useRef(null);
+    const tagElementsRef = useRef([]);
+
     return (
         <div className={styles.container}>
             <div className={styles.choiceContainer}>
                 <h3>Enter all of the choices divided by space(' ').
                     <br />Press Enter when you're done.
                 </h3>
-                <textarea placeholder="Enter your choices here..." id="textarea"></textarea>
-                <div id="tags"></div>
+                <textarea
+                    placeholder="Enter your choices here..."
+                    ref={textareaRef}>
+                </textarea>
+                <div ref={tagsContainerRef}>
+                    {tags.map((tag, index) => (
+                        <span key={index}
+                            className={styles.tag}>
+                            {tag}
+                        </span>
+                    ))}
+                </div>
             </div>
         </div>
     )
