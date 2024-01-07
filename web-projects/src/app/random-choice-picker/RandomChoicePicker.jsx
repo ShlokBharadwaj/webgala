f5d77d547c368bb35431fbd9483c51f819995925
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-07 10:05:34 +0530

    Add: Route for random choice picker

---
+++ web-projects/src/app/random-choice-picker/RandomChoicePicker.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './RandomChoicePicker.module.css';
+
+const RandomChoicePicker = () => {
+  return (
+    <div>RandomChoicePicker</div>
+  )
+}
+
+export default RandomChoicePicker;
