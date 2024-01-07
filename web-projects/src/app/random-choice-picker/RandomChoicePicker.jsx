4807ea10392845d496649811cfca2851e94746f0
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-07 10:12:15 +0530

    Add: JSX structure for Random Choice Picker

--- web-projects/src/app/random-choice-picker/RandomChoicePicker.jsx
+++ web-projects/src/app/random-choice-picker/RandomChoicePicker.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import styles from './RandomChoicePicker.module.css';
 
 const RandomChoicePicker = () => {
-  return (
-    <div>RandomChoicePicker</div>
-  )
+    return (
+        <div className={styles.container}>
+            <div className={styles.choiceContainer}>
+                <h3>Enter all of the choices divided by space(' ').
+                    <br />Press Enter when you're done.
+                </h3>
+                <textarea placeholder="Enter your choices here..." id="textarea"></textarea>
+                <div id="tags"></div>
+            </div>
+        </div>
+    )
 }
 
 export default RandomChoicePicker;
