9d5c56332d15757ca0c2725135c92ffc6e8c9597
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-08 15:03:28 +0530

    Add: JSX layout with Tailwind for Random Image Feed

--- web-projects/src/app/random-image-feed/RandomImageFeed.jsx
+++ web-projects/src/app/random-image-feed/RandomImageFeed.jsx
@@ -3,7 +3,10 @@ import styles from './RandomImageFeed.module.css';
 
 const RandomImageFeed = () => {
     return (
-        <div>RandomImageFeed</div>
+        <div className="bg-[#f6fff8] box-border flex flex-col items-center justify-center min-h-full overflow-visible m-0 text-black">
+            <h1 className="text-3xl mt-3 ml-0 mr-0 pt-5 text-center">Random Image Feed</h1>
+            <div className={`${styles.container} flex items-center content-center flex-wrap max-w-4xl`}></div>
+        </div>
     )
 }
 
