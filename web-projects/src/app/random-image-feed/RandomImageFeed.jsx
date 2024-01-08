79d7bb84de2751d52b23bdf63b765fd2352d8342
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-08 14:29:07 +0530

    Add: Route for Random Image Feed

---
+++ web-projects/src/app/random-image-feed/RandomImageFeed.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './RandomImageFeed.module.css';
+
+const RandomImageFeed = () => {
+    return (
+        <div>RandomImageFeed</div>
+    )
+}
+
+export default RandomImageFeed;
