c632215a9e5d97ed604a5a5a1fedf9c908b25ec8
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-10 17:25:37 +0530

    Add: Route for rotating navigation

---
+++ web-projects/src/app/rotating-navigation/RotatingNavigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './RotatingNavigation.module.css';
+
+const RotatingNavigation = () => {
+  return (
+    <div>RotatingNavigation</div>
+  )
+}
+
+export default RotatingNavigation;
