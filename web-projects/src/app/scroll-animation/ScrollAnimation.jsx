cada4f8bd093a5cf6077106700b9bc0eab56a76f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-12 13:15:46 +0530

    Add: Array to map through and display divs

--- web-projects/src/app/scroll-animation/ScrollAnimation.jsx
+++ web-projects/src/app/scroll-animation/ScrollAnimation.jsx
@@ -3,7 +3,14 @@ import styles from './ScrollAnimation.module.css';
 
 const ScrollAnimation = () => {
     return (
-        <div>ScrollAnimation</div>
+        <div className={styles.container}>
+            <h1>Scroll to see the Animation.</h1>
+            {[...Array(20)].map((_, index) => (
+                <div key={index} className={styles.box}>
+                    <h1>Some content</h1>
+                </div>
+            ))}
+        </div>
     )
 }
 
