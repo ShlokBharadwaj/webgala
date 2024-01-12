a76c4b4c6a268752f5978e0f7da71705ffeb8b15
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-12 12:35:51 +0530

    Add: Scroll Animation route

---
+++ web-projects/src/app/scroll-animation/ScrollAnimation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ScrollAnimation.module.css';
+
+const ScrollAnimation = () => {
+    return (
+        <div>ScrollAnimation</div>
+    )
+}
+
+export default ScrollAnimation;
