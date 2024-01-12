6b0117a0ff5e66c7a914a42dcd192c3efec80621
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-12 16:27:08 +0530

    Add: Check windows height to dynamically display divs

--- web-projects/src/app/scroll-animation/ScrollAnimation.jsx
+++ web-projects/src/app/scroll-animation/ScrollAnimation.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './ScrollAnimation.module.css';
 
 const ScrollAnimation = () => {
-    return (
-        <div className={styles.container}>
-            <h1>Scroll to see the Animation.</h1>
-            {[...Array(20)].map((_, index) => (
-                <div key={index} className={styles.box}>
-                    <h1>Some content</h1>
-                </div>
-            ))}
+  const boxesRef = useRef(null);
+  const [boxes, setBoxes] = useState([]);
+
+  const checkBoxes = () => {
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    boxes.forEach(box => {
+      const boxTop = box.getBoundingClientRect().top;
+
+      if (boxTop < triggerBottom) {
+        box.classList.add(styles.show);
+      } else {
+        box.classList.remove(styles.show);
+      }
+    });
+  };
+
+  useEffect(() => {
+    setBoxes(boxesRef.current.querySelectorAll(`.${styles.box}`));
+
+    window.addEventListener('scroll', checkBoxes);
+    checkBoxes();
+
+    return () => {
+      window.removeEventListener('scroll', checkBoxes);
+    };
+  }, []);
+
+  return (
+    <div ref={boxesRef} className={styles.container}>
+      <h1>Scroll to see the Animation.</h1>
+      {[...Array(20)].map((_, index) => (
+        <div key={index} className={styles.box}>
+          <h1>Some content</h1>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default ScrollAnimation;
