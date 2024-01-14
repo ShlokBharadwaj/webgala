592825b92cad3796674ca6856db55e848a1158e1
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-14 12:53:27 +0530

    Add: Split landing page route

---
+++ web-projects/src/app/split-landing-page/SplitLandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './SplitLandingPage.module.css';
+
+const SplitLandingPage = () => {
+    return (
+        <div>SplitLandingPage</div>
+    )
+}
+
+export default SplitLandingPage;
