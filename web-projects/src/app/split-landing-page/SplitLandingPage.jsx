10d4360cfc101d12cef948c89b9d3ba7518b1669
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-15 16:57:05 +0530

    Add: Hyperlink to X and Instagram

--- web-projects/src/app/split-landing-page/SplitLandingPage.jsx
+++ web-projects/src/app/split-landing-page/SplitLandingPage.jsx
@@ -3,7 +3,18 @@ import styles from './SplitLandingPage.module.css';
 
 const SplitLandingPage = () => {
     return (
-        <div>SplitLandingPage</div>
+        <div className={styles.container}>
+            <div className={styles.childContainer}>
+                <div className={`${styles.split} ${styles.left}`}>
+                    <h1>X</h1>
+                    <a href="https://x.com/" target="_blank" className={styles.btn}>Click here</a>
+                </div>
+                <div className={`${styles.split} ${styles.right}`}>
+                    <h1>Instagram</h1>
+                    <a href="https://instagram.com/" target="_blank" className={styles.btn}>Click here</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
