5918a07b299eca80b6e0e503c97642b0e6a610a3
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-15 17:43:27 +0530

    Add: State to check for mouse enter and leave

--- web-projects/src/app/split-landing-page/SplitLandingPage.jsx
+++ web-projects/src/app/split-landing-page/SplitLandingPage.jsx
@@ -7,16 +7,38 @@ const SplitLandingPage = () => {
     const [isHoverLeft, setHoverLeft] = useState(false);
     const [isHoverRight, setHoverRight] = useState(false);
 
+    const handleMouseEnterLeft = () => {
+        setHoverLeft(true);
+    };
+
+    const handleMouseLeaveLeft = () => {
+        setHoverLeft(false);
+    };
+
+    const handleMouseEnterRight = () => {
+        setHoverRight(true);
+    };
+
+    const handleMouseLeaveRight = () => {
+        setHoverRight(false);
+    };
+
     return (
         <div className={`${styles.container} ${isHoverLeft ? styles.hoverLeft : ''} ${isHoverRight ? styles.hoverRight : ''}`} ref={containerRef}>
             <div className={styles.childContainer}>
-                <div className={`${styles.split} ${styles.left}`}>
+                <div className={`${styles.split} ${styles.left}`} onMouseEnter={handleMouseEnterLeft} onMouseLeave={handleMouseLeaveLeft}>
                     <h1>X</h1>
-                    <a href="https://x.com/" target="_blank" className={styles.btn}>Click here</a>
+                    <a
+                        href="https://x.com/"
+                        target="_blank"
+                        className={styles.btn}>Click here</a>
                 </div>
-                <div className={`${styles.split} ${styles.right}`}>
+                <div className={`${styles.split} ${styles.right}`} onMouseEnter={handleMouseEnterRight} onMouseLeave={handleMouseLeaveRight}>
                     <h1>Instagram</h1>
-                    <a href="https://instagram.com/" target="_blank" className={styles.btn}>Click here</a>
+                    <a
+                        href="https://instagram.com/"
+                        target="_blank"
+                        className={styles.btn}>Click here</a>
                 </div>
             </div>
         </div>
