7768c60a1b4d844f2d132011f6a66115e6abb16d
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-16 17:02:46 +0530

    Add: Responsive and image hover, active effects

--- web-projects/src/app/random-image-feed/RandomImageFeed.jsx
+++ web-projects/src/app/random-image-feed/RandomImageFeed.jsx
@@ -29,11 +29,11 @@ const RandomImageFeed = () => {
     }
 
     return (
-        <div className="bg-[#f6fff8] box-border flex flex-col items-center justify-center min-h-full overflow-visible m-0 text-black">
+        <div className="bg-[#011627] box-border flex flex-col items-center justify-center min-h-full overflow-visible m-0 text-white">
             <h1 className="text-3xl mt-3 ml-0 mr-0 pt-5 text-center font-bold">Random Image Feed</h1>
             <div className={`${styles.container} flex items-center content-center flex-wrap max-w-5xl mt-3`}>
                 {images.map((imageURL, index) => (
-                    <img key={index} src={imageURL} alt={`Random Image ${index + 1}`} />
+                    <img key={index} src={imageURL} alt={`Random Image ${index + 1}`} className={styles.image} />
                 ))}
             </div>
         </div>
