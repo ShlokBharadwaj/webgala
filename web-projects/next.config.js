1b040627c403a4aa9e22261defb1d1ed6974a178
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-18 13:41:21 +0530

    Update: Config for Audio

--- web-projects/next.config.js
+++ web-projects/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(mp3)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'audio/',
+          },
+        },
+      });
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
