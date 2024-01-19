f411d7821899cfce4a9063fb2762b7df7f8bf62f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-19 12:42:29 +0530

    Add: Style to Audio buttons and responsive

--- web-projects/src/app/sound-board/SoundBoard.jsx
+++ web-projects/src/app/sound-board/SoundBoard.jsx
@@ -2,11 +2,11 @@ import React, { useRef } from 'react';
 import styles from './SoundBoard.module.css';
 
 const audioFiles = [
-    { name: 'Audio 1', file: '/audio/best.mp3' },
-    { name: 'Audio 2', file: '/audio/notif1.mp3' },
-    { name: 'Audio 3', file: '/audio/notif2.mp3' },
-    { name: 'Audio 4', file: '/audio/notif3.mp3' },
-    { name: 'Audio 5', file: '/audio/pew.mp3' },
+    { name: 'Audio 1', file: '/audio/notif1.mp3' },
+    { name: 'Audio 2', file: '/audio/notif2.mp3' },
+    { name: 'Audio 3', file: '/audio/notif3.mp3' },
+    { name: 'Audio 4', file: '/audio/pew.mp3' },
+    { name: 'Audio 5', file: '/audio/best.mp3' },
 ];
 
 const SoundBoard = () => {
