4df0874774bd6c973c6c9434ff92cfeb3f550888
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-19 12:25:53 +0530

    Add: Array of objects to map through Audios

--- web-projects/src/app/sound-board/SoundBoard.jsx
+++ web-projects/src/app/sound-board/SoundBoard.jsx
@@ -1,11 +1,13 @@
 import React, { useRef } from 'react';
 import styles from './SoundBoard.module.css';
 
-import best from '../../../public/audio/best.mp3';
-import notif1 from '../../../public/audio/notif1.mp3';
-import notif2 from '../../../public/audio/notif2.mp3';
-import notif3 from '../../../public/audio/notif3.mp3';
-import pew from '../../../public/audio/pew.mp3';
+const audioFiles = [
+    { name: 'Audio 1', file: '/audio/best.mp3' },
+    { name: 'Audio 2', file: '/audio/notif1.mp3' },
+    { name: 'Audio 3', file: '/audio/notif2.mp3' },
+    { name: 'Audio 4', file: '/audio/notif3.mp3' },
+    { name: 'Audio 5', file: '/audio/pew.mp3' },
+];
 
 const SoundBoard = () => {
     const audioRef = useRef(null);
@@ -17,12 +19,11 @@ const SoundBoard = () => {
 
     return (
         <div className={styles.container}>
-            <button onClick={() => playAudio('/audio/best.mp3')}>Play Best Audio</button>
-            <button onClick={() => playAudio('/audio/notif1.mp3')}>Play Notification 1</button>
-            <button onClick={() => playAudio('/audio/notif2.mp3')}>Play Notification 2</button>
-            <button onClick={() => playAudio('/audio/notif3.mp3')}>Play Notification 3</button>
-            <button onClick={() => playAudio('/audio/pew.mp3')}>Play Pew Audio</button>
-
+            {audioFiles.map((audio, index) => (
+                <button key={index} onClick={() => playAudio(audio.file)} className={styles.btn}>
+                    {`Play ${audio.name}`}
+                </button>
+            ))}
             <audio ref={audioRef} />
         </div>
     );
