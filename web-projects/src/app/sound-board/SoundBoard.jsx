10cbd0ec3fb51b18413f540a88114e8b4b9b27d7
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-19 11:26:29 +0530

    Update: Audio play functionality

--- web-projects/src/app/sound-board/SoundBoard.jsx
+++ web-projects/src/app/sound-board/SoundBoard.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import styles from './SoundBoard.module.css';
+
 import best from '../../../public/audio/best.mp3';
 import notif1 from '../../../public/audio/notif1.mp3';
 import notif2 from '../../../public/audio/notif2.mp3';
@@ -16,11 +17,12 @@ const SoundBoard = () => {
 
     return (
         <div className={styles.container}>
-            <button onClick={() => playAudio(best)}>Play Best Audio</button>
-            <button onClick={() => playAudio(notif1)}>Play Notification 1</button>
-            <button onClick={() => playAudio(notif2)}>Play Notification 2</button>
-            <button onClick={() => playAudio(notif3)}>Play Notification 3</button>
-            <button onClick={() => playAudio(pew)}>Play Pew Audio</button>
+            <button onClick={() => playAudio('/audio/best.mp3')}>Play Best Audio</button>
+            <button onClick={() => playAudio('/audio/notif1.mp3')}>Play Notification 1</button>
+            <button onClick={() => playAudio('/audio/notif2.mp3')}>Play Notification 2</button>
+            <button onClick={() => playAudio('/audio/notif3.mp3')}>Play Notification 3</button>
+            <button onClick={() => playAudio('/audio/pew.mp3')}>Play Pew Audio</button>
+
             <audio ref={audioRef} />
         </div>
     );
