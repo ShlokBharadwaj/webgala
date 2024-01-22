7aaa722225c88d183169d36a8331188603a05925
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-22 18:13:53 +0530

    Add: Route for Testimonail Box Switcher

---
+++ web-projects/src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './TestimonialBoxSwitcher.module.css';
+
+const TestimonialBoxSwitcher = () => {
+  return (
+    <div>TestimonialBoxSwitcher</div>
+  )
+}
+
+export default TestimonialBoxSwitcher;
