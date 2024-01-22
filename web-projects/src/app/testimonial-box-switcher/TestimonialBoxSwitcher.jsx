50e1cba36ab0e77f54b359cd53c6018877c30dae
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-22 20:17:39 +0530

    Add: Basic style for Testimonial using Tailwind

--- web-projects/src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
+++ web-projects/src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './TestimonialBoxSwitcher.module.css';
 
 const TestimonialBoxSwitcher = () => {
-  return (
-    <div className={`${styles.container} box-border`}>TestimonialBoxSwitcher</div>
-  )
+    return (
+        <div className={`${styles.container} box-border flex flex-col items-center justify-center min-h-screen overflow-hidden p-3 bg-[#f1faee] text-white`}>
+            <div className="testimonial-container bg-[#457b9d] rounded-lg m-5 p-12 max-w-3xl relative">TestimonialBoxSwitcher</div>
+        </div>
+    )
 }
 
 export default TestimonialBoxSwitcher;
