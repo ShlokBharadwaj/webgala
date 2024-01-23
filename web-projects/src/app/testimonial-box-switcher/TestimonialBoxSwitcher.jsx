d9438954b6a222c7998e432f6ecae4dad167a524
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-23 10:15:17 +0530

    Add: User details for Testimonial

--- web-projects/src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
+++ web-projects/src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
@@ -13,6 +13,10 @@ const TestimonialBoxSwitcher = () => {
                 <p className="testimonial leading-7 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsam recusandae neque, id dicta facilis repellendus quod, quis alias ullam vero repudiandae, repellat dolor dolore!</p>
                 <div className="user flex items-center justify-center pt-4">
                     <img src="https://source.unsplash.com/random/?portrait" alt="user" className="user-image rounded-[50%] h-20 w-20 object-cover hover:animate-pulse" />
+                    <div className="user-details m-3">
+                        <h4 className="username m-0 font-bold">Sebastian West</h4>
+                        <p className="role font-normal">Senior HR</p>
+                    </div>
                 </div>
             </div>
         </div>
