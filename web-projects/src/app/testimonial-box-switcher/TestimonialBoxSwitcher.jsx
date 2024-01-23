6e3d7dc9dd8865d032d009c8355ee72ae6d9c9ca
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-23 11:48:24 +0530

    Add: Array of objects with testimonial data

--- web-projects/src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
+++ web-projects/src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
@@ -1,26 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./TestimonialBoxSwitcher.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 
 const TestimonialBoxSwitcher = () => {
+    const [testimonialIndex, setTestimonialIndex] = useState(0);
+
+    const testimonials = [
+        {
+            name: 'Jonathan Walters',
+            position: 'Framer',
+            photo: 'https://source.unsplash.com/random/?portrait,india',
+            text: ' Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ',
+        },
+        {
+            name: 'Miyah Myles',
+            position: 'Marketing',
+            photo: 'https://source.unsplash.com/random/?portrait,women',
+            text: ' I\'ve been interested in coding for a while but never taken the jump, until now. I couldn\'t recommend this course enough. I\'m now in the job of my dreams and so excited about the future. ',
+        },
+        {
+            name: 'June Cha',
+            position: 'Software Engineer',
+            photo: 'https://source.unsplash.com/random/?portrait,men',
+            text: ' If you want to lay the best foundation possible I\'d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ',
+        },
+        {
+            name: 'Iida Niskanen',
+            position: 'Data Entry',
+            photo: 'https://source.unsplash.com/random/?portrait,women,men',
+            text: ' I was an EMT for many years before I joined the bootcamp. I\'ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I\'ve successfully switched careers, working as a Software Engineer at a VR startup. ',
+        },
+        {
+            name: 'Renee Sims',
+            position: 'Receptionist',
+            photo: 'https://source.unsplash.com/random/?portrait,men,women,people',
+            text: ' I\'m a receptionist at a law firm and was looking for resources to better my knowledge of the field. This was a fantastic course that gave me everything I needed to know in a comprehensive and digestible way. ',
+        },
+        {
+            name: 'Sasha Ho',
+            position: 'Architect',
+            photo: 'https://source.unsplash.com/random/?portrait,men,indian',
+            text: ' Before joining the bootcamp, I\'ve never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I\'ve often referred to it during interviews as an example of my developent experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend! ',
+        },
+        {
+            name: 'Veeti Seppanen',
+            position: 'Director',
+            photo: 'https://source.unsplash.com/random/?portrait,women,indian',
+            text: ' The team was very supportive and kept me motivated. I learned a lot in a short period of time, and I now feel so confident about starting up as a professional developer. ',
+        },
+    ];
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTestimonialIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+        }, 7000);
+
+        return () => clearInterval(intervalId);
+    }, [testimonials.length]);
+
+    const { name, position, photo, text } = testimonials[testimonialIndex];
+
     return (
         <div className={`${styles.container} box-border flex flex-col items-center justify-center min-h-screen overflow-hidden p-3 bg-[#f1faee] text-white`}>
-            <div className="testimonialContainer bg-[#457b9d] rounded-lg m-5 p-12 max-w-3xl relative">
+            <div className="bg-[#457b9d] rounded-lg m-5 p-12 max-w-3xl relative">
                 <div className={`${styles.progressBar} bg-white h-1 w-full m-3`}></div>
                 <FontAwesomeIcon icon={faQuoteRight} className="text-white text-3xl absolute top-5 left-10"></FontAwesomeIcon>
                 <FontAwesomeIcon icon={faQuoteLeft} className="text-white text-3xl absolute bottom-5 right-10"></FontAwesomeIcon>
-                <p className="testimonial leading-7 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsam recusandae neque, id dicta facilis repellendus quod, quis alias ullam vero repudiandae, repellat dolor dolore!</p>
-                <div className="user flex items-center justify-center pt-4">
-                    <img src="https://source.unsplash.com/random/?portrait" alt="Random user portrait from unsplash.com" className="userImage rounded-[50%] h-20 w-20 object-cover hover:animate-pulse" />
-                    <div className="user-details m-3">
-                        <h4 className="username m-0 font-bold">Sebastian West</h4>
-                        <p className="role font-normal">Senior HR</p>
+                <p className="leading-7 text-justify">{text}</p>
+                <div className="flex items-center justify-center pt-4">
+                    <img src={photo} alt={`Portrait of ${name}`} className="rounded-[50%] h-20 w-20 object-cover hover:animate-pulse" />
+                    <div className="m-3">
+                        <h4 className="m-0 font-bold">{name}</h4>
+                        <p className="font-normal">{position}</p>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default TestimonialBoxSwitcher;
