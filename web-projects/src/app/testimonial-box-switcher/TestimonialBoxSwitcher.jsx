6be878e371e5591d518db488023fc24fae24b5ba
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-23 10:22:56 +0530

    Add: Animation for next testimonial

--- web-projects/src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
+++ web-projects/src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
@@ -7,7 +7,7 @@ const TestimonialBoxSwitcher = () => {
     return (
         <div className={`${styles.container} box-border flex flex-col items-center justify-center min-h-screen overflow-hidden p-3 bg-[#f1faee] text-white`}>
             <div className="testimonial-container bg-[#457b9d] rounded-lg m-5 p-12 max-w-3xl relative">
-                <div className="progress-bar bg-white h-1 w-full m-3"></div>
+                <div className={`${styles.progressBar} bg-white h-1 w-full m-3`}></div>
                 <FontAwesomeIcon icon={faQuoteRight} className="text-white text-3xl absolute top-5 left-10"></FontAwesomeIcon>
                 <FontAwesomeIcon icon={faQuoteLeft} className="text-white text-3xl absolute bottom-5 right-10"></FontAwesomeIcon>
                 <p className="testimonial leading-7 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsam recusandae neque, id dicta facilis repellendus quod, quis alias ullam vero repudiandae, repellat dolor dolore!</p>
