5babb4ca383ab021bf5c8125031c14d14b72ad91
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-23 14:08:20 +0530

    Add: JSX structure for Clock

--- web-projects/src/app/theme-clock/ThemeClock.jsx
+++ web-projects/src/app/theme-clock/ThemeClock.jsx
@@ -3,7 +3,19 @@ import styles from './ThemeClock.module.css';
 
 const ThemeClock = () => {
     return (
-        <div>ThemeClock</div>
+        <div className={styles.container}>
+            <button className={styles.toggle}>Dark Mode</button>
+            <div className={styles.clockContainer}>
+                <div className={styles.clock}>
+                    <div className={`${styles.needle} ${styles.hour}`}></div>
+                    <div className={`${styles.needle} ${styles.minute}`}></div>
+                    <div className={`${styles.needle} ${styles.second}`}></div>
+                    <div className={`${styles.needle} ${styles.point}`}></div>
+                </div>
+                <div className={styles.time}></div>
+                <div className={styles.date}></div>
+            </div>
+        </div>
     )
 }
 
