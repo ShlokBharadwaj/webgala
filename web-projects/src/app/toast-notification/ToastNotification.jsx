8d5cdc486d69b4c481ad0d0f5d908d4ce4728666
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-24 16:14:28 +0530

    Add: Get random message and type from Array and Style

--- web-projects/src/app/toast-notification/ToastNotification.jsx
+++ web-projects/src/app/toast-notification/ToastNotification.jsx
@@ -2,10 +2,46 @@ import React from 'react';
 import styles from './ToastNotification.module.css';
 
 const ToastNotification = () => {
+
+    const messages = [
+        {
+            id: 1,
+            text: 'Message One',
+            type: 'success'
+        },
+        {
+            id: 2,
+            text: 'Message Two',
+            type: 'error'
+        },
+        {
+            id: 3,
+            text: 'Message Three',
+            type: 'warning'
+        },
+        {
+            id: 4,
+            text: 'Message Four',
+            type: 'info'
+        }
+    ];
+
+    const getRandomMessage = () => {
+        return messages[Math.floor(Math.random() * messages.length)];
+    };
+
+    const getRandomType = () => {
+        return ['success', 'error', 'warning', 'info'][Math.floor(Math.random() * 4)];
+    };
+
+
+    const createNotification = () => { }
+
+
     return (
         <div className={styles.container}>
             <div className={styles.toasts}></div>
-            <button className={styles.btn}>Show Notification</button>
+            <button className={styles.btn} onClick={createNotification}>Show Notification</button>
         </div>
     )
 }
