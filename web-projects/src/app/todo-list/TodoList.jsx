3a055e029eee719321847e540b0a236c2f404355
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-26 19:37:30 +0530

    Add: Details for Complete and Delete todos

--- web-projects/src/app/todo-list/TodoList.jsx
+++ web-projects/src/app/todo-list/TodoList.jsx
@@ -9,6 +9,8 @@ const TodoList = () => {
                 <input type="text" className="input border-none text-[#444] text-4xl pt-4 pb-4 pl-8 pr-8 block w-full focus:outline-[#0077b6] opacity-50 text-center" id="input" placeholder="your todos here" autoComplete="off" />
                 <ul className="todos bg-white p-0 m-0" id="todos"></ul>
             </form>
+            <small className="text-[#b4b4b4] mt-12 text-center text-lg">left click to toggle complete</small>
+            <small className="text-[#b4b4b4] text-center text-lg">right click to delete</small>
         </div>
     )
 }
