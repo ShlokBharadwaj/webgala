b1253c4080eab330cb59cee66e2d57ec1aaf4590
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-27 12:40:51 +0530

    Add: Store and display totos from local storage

--- web-projects/src/app/todo-list/TodoList.jsx
+++ web-projects/src/app/todo-list/TodoList.jsx
@@ -11,18 +11,65 @@ const TodoList = () => {
         addTodo();
     };
 
-    const addTodo = (todo) => { };
+    const addTodo = (todo) => {
+        let todoTextValue = todo ? todo.text : todoText;
+
+        if (todo) {
+            todoTextValue = todo.text;
+        }
+
+        if (todoTextValue.trim() === '') {
+            return;
+        }
+
+        if (todoTextValue) {
+            const newTodos = [...todos, { text: todoTextValue, complete: false }];
+            setTodos(newTodos);
+            setTodoText('');
+        }
+    };
+
+    const updateLocalStorage = () => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    };
 
     const handleInputChange = (e) => {
         setTodoText(e.target.value);
     }
 
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem('todos'));
+        if (storedTodos) {
+            setTodos(storedTodos);
+        }
+    }, []);
+
+    useEffect(() => {
+        updateLocalStorage();
+    }, [todos]);
+
     return (
         <div className={`${styles.container} bg-[#f8f9fa] text-black flex flex-col items-center justify-center min-h-screen m-0 box-border`}>
             <h1 className="text-[11rem] text-[#0077b6] text-center opacity-50">todos</h1>
             <form className="shadow-2xl max-w-full w-96" onSubmit={handleSubmit}>
-                <input type="text" className="border-none text-[#444] text-4xl pt-4 pb-4 pl-8 pr-8 block w-full focus:outline-[#0077b6] opacity-50 text-center" placeholder="your todos here" autoComplete="off" value={todoText} onChange={handleInputChange} />
-                <ul className="todos bg-white p-0 m-0" id="todos"></ul>
+                <input
+                    type="text"
+                    className="border-none text-[#444] text-4xl pt-4 pb-4 pl-8 pr-8 block w-full focus:outline-[#0077b6] opacity-50 text-center"
+                    placeholder="your todos here"
+                    autoComplete="off"
+                    value={todoText}
+                    onChange={handleInputChange} />
+                <ul className={`${styles.todos} bg-white p-0 m-0`}>
+                    {todos.map((todo, index) => (
+                        <li key={index}
+                            className={`${styles.todo} border-t border-[#e0e0e0] text-[#444] text-2xl pt-4 pb-4 pl-8 pr-8 block w-full focus:outline-[#0077b6] opacity-50 text-center cursor-pointer`}
+                            onClick={() => toggleTodoClick(index)}
+                            onContextMenu={(e) => handleTodoContextMenu(e, index)}
+                        >
+                            {todo.text}
+                        </li>
+                    ))}
+                </ul>
             </form>
             <small className="text-[#b4b4b4] mt-12 text-center text-lg">left click to toggle complete</small>
             <small className="text-[#b4b4b4] text-center text-lg">right click to delete</small>
