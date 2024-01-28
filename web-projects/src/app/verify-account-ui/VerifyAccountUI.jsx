ee82159fc9f01d567c9c855ed77426c3a4e5c38f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-28 18:41:05 +0530

    Add: Route for Verify Account UI

---
+++ web-projects/src/app/verify-account-ui/VerifyAccountUI.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './VerifyAccountUI.module.css';
+
+const VerifyAccountUI = () => {
+    return (
+        <div>VerifyAccountUI</div>
+    )
+}
+
+export default VerifyAccountUI;
