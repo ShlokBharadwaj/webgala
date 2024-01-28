1b5b644c911d1e1133069cf86f69277acdb0afe2
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-28 19:13:30 +0530

    Add: Basic style for Verify Account UI

--- web-projects/src/app/verify-account-ui/VerifyAccountUI.jsx
+++ web-projects/src/app/verify-account-ui/VerifyAccountUI.jsx
@@ -3,7 +3,11 @@ import styles from './VerifyAccountUI.module.css';
 
 const VerifyAccountUI = () => {
     return (
-        <div>VerifyAccountUI</div>
+        <div className={styles.container}>
+            <div className="bg-[#caf0f8] rounded-md p-10 text-center shadow-2xl max-w-[90%]">
+                <h1 className="text-4xl text-black">Verify your Account</h1>
+            </div>
+        </div>
     )
 }
 
