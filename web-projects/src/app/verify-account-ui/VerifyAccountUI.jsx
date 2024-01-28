8194cfe97706fefc896422ada2e3bf3add4ee717
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-28 19:29:33 +0530

    Add: Map through and display number input

--- web-projects/src/app/verify-account-ui/VerifyAccountUI.jsx
+++ web-projects/src/app/verify-account-ui/VerifyAccountUI.jsx
@@ -4,11 +4,26 @@ import styles from './VerifyAccountUI.module.css';
 const VerifyAccountUI = () => {
     return (
         <div className={styles.container}>
-            <div className="bg-[#caf0f8] rounded-md p-10 text-center shadow-2xl max-w-[90%]">
+            <div className={`bg-[#caf0f8] rounded-md p-10 text-center shadow-2xl max-w-[90%] text-black`}>
                 <h1 className="text-4xl text-black">Verify your Account</h1>
+                <p className="text-sm text-gray-700 pt-1">We messaged you the six-digit code to your registered mobile number: +91 867****762.</p>
+                <p className="text-sm text-gray-700 pt-1">Please enter the code below to confirm your mobile number.</p>
+                <div className={`${styles.numberContainer}`}>
+                    {[...Array(6)].map((_, index) => (
+                        <input
+                            key={index}
+                            type="number"
+                            className={styles.number}
+                            placeholder="0"
+                            min={0}
+                            max={9}
+                            required
+                        />
+                    ))}
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default VerifyAccountUI;
