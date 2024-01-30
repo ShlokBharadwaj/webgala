d08e4411055ea8a02f0addfe71c5e41d81a54954
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-30 11:02:56 +0530

    Update: Use React hook for Animated Countdowns

--- web-projects/src/app/animated-countdowns/AnimatedCountdowns.jsx
+++ web-projects/src/app/animated-countdowns/AnimatedCountdowns.jsx
@@ -1,63 +1,69 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styles from './AnimatedCountdowns.module.css';
 
 const AnimatedCountdowns = () => {
-
     const [animationCompleted, setAnimationCompleted] = useState(false);
+    const numsRef = useRef([]);
+    const counterRef = useRef(null);
+    const finalMessageRef = useRef(null);
+
+    const resetDOM = () => {
+        const counter = counterRef.current;
+        const finalMessage = finalMessageRef.current;
+
+        counter.classList.remove(styles.hide);
+        finalMessage.classList.remove(styles.show);
+
+        numsRef.current.forEach((num) => {
+            num.classList.value = '';
+        });
+
+        numsRef.current[0].classList.add(styles.in);
+    };
+
+    const runAnimation = () => {
+        numsRef.current.forEach((num, idx) => {
+            const nextToLast = numsRef.current.length - 1;
+
+            num.addEventListener('animationend', (e) => {
+                if (e.animationName === styles.goIn && idx !== nextToLast) {
+                    num.classList.remove(styles.in);
+                    num.classList.add(styles.out);
+                } else if (e.animationName === styles.goOut && num.nextElementSibling) {
+                    num.nextElementSibling.classList.add(styles.in);
+                } else {
+                    setAnimationCompleted(true);
+                    counterRef.current.classList.add(styles.hide);
+                    finalMessageRef.current.classList.add(styles.show);
+                }
+            });
+        });
+    };
 
     useEffect(() => {
-        const nums = document.querySelectorAll(`.${styles.nums} span`);
-        const counter = document.querySelector(`.${styles.counter}`);
-        const finalMessage = document.querySelector(`.${styles.final}`);
-        const replay = document.querySelector(`#${styles.replay}`);
-
         if (!animationCompleted) {
             runAnimation();
         }
 
-        function resetDOM() {
-            counter.classList.remove(styles.hide);
-            finalMessage.classList.remove(styles.show);
-
-            nums.forEach((num) => {
-                num.classList.value = '';
+        return () => {
+            numsRef.current.forEach((num) => {
+                num.removeEventListener('animationend', null);
             });
-
-            nums[0].classList.add(styles.in);
-        }
-
-        function runAnimation() {
-            nums.forEach((num, idx) => {
-                const nextToLast = nums.length - 1;
-
-                num.addEventListener('animationend', (e) => {
-                    if (e.animationName === styles.goIn && idx !== nextToLast) {
-                        num.classList.remove(styles.in);
-                        num.classList.add(styles.out);
-                    } else if (e.animationName === styles.goOut && num.nextElementSibling) {
-                        num.nextElementSibling.classList.add(styles.in);
-                    } else {
-                        setAnimationCompleted(true);
-                        counter.classList.add(styles.hide);
-                        finalMessage.classList.add(styles.show);
-                    }
-                });
-            });
-        }
-
-        replay.addEventListener('click', () => {
-            setAnimationCompleted(false);
-            resetDOM();
-            runAnimation();
-        });
+        };
     }, [animationCompleted]);
+
+    const handleReplay = () => {
+        setAnimationCompleted(false);
+        resetDOM();
+        runAnimation();
+    };
 
     return (
         <div className={styles.main}>
-            <div className={styles.counter}>
-                <div className={styles.nums}>
+            <div className={`${styles.counter}`} ref={counterRef}>
+                <div className={styles.nums} ref={(el) => el && (numsRef.current = [...el.children])}>
                     <span className={styles.in}>3</span>
                     <span>2</span>
                     <span>1</span>
@@ -66,14 +72,14 @@ const AnimatedCountdowns = () => {
                 <h4 className={styles.h4}>Get Ready!</h4>
             </div>
 
-            <div className={styles.final}>
+            <div className={`${styles.final}`} ref={finalMessageRef}>
                 <h1 className={styles.h1}>GO</h1>
-                <button id={styles.replay}>
+                <button id={styles.replay} onClick={handleReplay}>
                     <span>Replay</span>
                 </button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default AnimatedCountdowns;
