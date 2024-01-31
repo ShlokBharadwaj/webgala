4be75544c6c66da32852464e6482de611997c6d5
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-31 14:40:22 +0530

    Fix: Exhaustive deps lint rule for Hoverboard

--- src/app/hoverboard/Hoverboard.jsx
+++ src/app/hoverboard/Hoverboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import styles from './Hoverboard.module.css';
 
 const Hoverboard = () => {
@@ -7,6 +7,12 @@ const Hoverboard = () => {
     const colors = ["#9b2226", "#ae2012", "#bb3e03", "#ca6702", "#ee9b00", "#e9d8a6", "#94d2bd", "#0a9396", "#005f73", "#001219", "#ffba08", "#7400b8", "#55a630", "#ffea00"];
     const SQUARES = 500;
     const [squares, setSquares] = useState([]);
+    
+    const setColor = useCallback((element) => {
+        const color = getRandomColor();
+        element.style.background = color;
+        element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+    }, []);
 
     useEffect(() => {
 
@@ -28,17 +34,12 @@ const Hoverboard = () => {
         ));
 
         setSquares(squares);
-    }, []);
+    }, [setColor]);
 
     const getRandomColor = () => {
         return colors[Math.floor(Math.random() * colors.length)];
     };
 
-    const setColor = (element) => {
-        const color = getRandomColor();
-        element.style.background = color;
-        element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-    };
 
     const removeColor = (element) => {
         element.style.background = '#1d1d1d';
