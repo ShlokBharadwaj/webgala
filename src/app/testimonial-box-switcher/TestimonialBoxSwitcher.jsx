3e70feff43cc1962f63cb9f96fb63819aa4d25f9
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-31 17:13:24 +0530

    Update: Use Next images for Testimonial box switcher

--- src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
+++ src/app/testimonial-box-switcher/TestimonialBoxSwitcher.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from "./TestimonialBoxSwitcher.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 
 const TestimonialBoxSwitcher = () => {
     const [testimonials, setTestimonials] = useState([]);
@@ -45,7 +46,7 @@ const TestimonialBoxSwitcher = () => {
                         <FontAwesomeIcon icon={faQuoteLeft} className="text-white text-3xl absolute bottom-5 right-10"></FontAwesomeIcon>
                         <p className="leading-7 text-justify">{text}</p>
                         <div className="flex items-center justify-center pt-4">
-                            <img src={photo} alt={`Portrait of ${name}`} className="rounded-[50%] h-20 w-20 object-cover hover:animate-pulse" />
+                            <Image src={photo} alt={`Portrait of ${name}`} className="rounded-[50%] h-20 w-20 object-cover hover:animate-pulse" height={100} width={100}/>
                             <div className="m-3">
                                 <h4 className="m-0 font-bold">{name}</h4>
                                 <p className="font-normal">{position}</p>
