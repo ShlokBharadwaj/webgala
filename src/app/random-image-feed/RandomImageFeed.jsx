86a4d85949fae4ea6c456ca2d29be99bcaa4c721
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-31 17:08:57 +0530

    Fix: exhaustive-deps and optimize image load

--- src/app/random-image-feed/RandomImageFeed.jsx
+++ src/app/random-image-feed/RandomImageFeed.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import styles from './RandomImageFeed.module.css';
+import Image from "next/image";
+
+const getRandomNr = () => {
+    return Math.floor(Math.random() * 10) + 300;
+};
+
+const getRandomSize = () => {
+    return `${getRandomNr()}x${getRandomNr()}`;
+};
 
 const RandomImageFeed = () => {
     const [images, setImages] = useState([]);
@@ -20,20 +29,12 @@ const RandomImageFeed = () => {
         fetchImages();
     }, []);
 
-    const getRandomSize = () => {
-        return `${getRandomNr()}x${getRandomNr()}`;
-    };
-
-    const getRandomNr = () => {
-        return Math.floor(Math.random() * 10) + 300;
-    };
-
     return (
         <div className="bg-[#011627] box-border flex flex-col items-center justify-center min-h-full overflow-visible m-0 text-white">
             <h1 className="text-3xl mt-3 ml-0 mr-0 pt-5 text-center font-bold">Random Image Feed</h1>
             <div className={`${styles.container} flex items-center content-center flex-wrap max-w-5xl mt-3`}>
                 {images.map((imageURL, index) => (
-                    <img key={index} src={imageURL} alt={`Random Image ${index + 1}`} className={styles.image} />
+                    <Image key={index} src={imageURL} alt={`Random Image ${index + 1}`} className={styles.image} height={100} width={100} />
                 ))}
             </div>
         </div>
