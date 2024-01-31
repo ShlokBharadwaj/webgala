36837fcf466e244445fb3713909161b904328b2b
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-01-31 13:27:41 +0530

    Fix: Escape sequence for Dad Jokes

--- src/app/dad-jokes/DadJokes.jsx
+++ src/app/dad-jokes/DadJokes.jsx
@@ -37,7 +37,7 @@ const DadJokes = () => {
   return (
     <div className={styles.main}>
       <div className={styles.container}>
-        <h3 className={styles.h3}>Don't Laugh Challenge</h3>
+        <h3 className={styles.h3}>Don&apos;t Laugh Challenge</h3>
         <div className={styles.joke} ref={jokeRef}>
           loading jokes...
         </div>
